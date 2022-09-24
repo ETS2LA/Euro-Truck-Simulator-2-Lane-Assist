0df6d20b2e29cecd70714a6380136d2935ce2d1e
Tumppi066 <tuomas.keidasto@gmail.com>
2022-09-24 11:48:25 +0300

    Fixed bugs

--- updater.py
+++ updater.py
@@ -7,6 +7,13 @@ import zipfile
 import tkinter as tk
 import webbrowser
 
+def PrintGreen(text):
+    print("\033[92m {}\033[00m" .format(text))
+
+def PrintRed(text):
+    print("\033[91m {}\033[00m" .format(text))
+
+# Helper functions for tkinter
 def AddLabel(text, frame, font = "Calibri", size = 12, color = "white"):
     labelVal = tk.StringVar()
     labelVal.set(text)
@@ -14,10 +21,12 @@ def AddLabel(text, frame, font = "Calibri", size = 12, color = "white"):
     label.pack()
     return labelVal
 
+# Helper functions for tkinter
 def AddButton(name, callback, frame):
     button = tk.Button(frame, text=name, command=callback, width=100, fg="white", bg="#1c1c1c")
     button.pack()
 
+# Helper functions for tkinter
 def AddEntry(name, frame, width = 100):
     entry = tk.Entry(frame, text = name, width=width, fg="white", bg="#1c1c1c")
     entry.pack()
@@ -25,10 +34,11 @@ def AddEntry(name, frame, width = 100):
 
 # Default paths
 hostname = "github.com" # For checking internet
-dest = "Euro-Truck-Simulator-2-Lane-Assist-main"
 localVersion = 0
 newestVersion = 0
 changeLog = ""
+branch = "main"
+dest = "Euro-Truck-Simulator-2-Lane-Assist-{}".format(branch)
 
 # Just so there are no errors
 newestLabel = 0
@@ -37,8 +47,8 @@ root = 0
 installButton = 0
 warningLabel = 0
 changeLogButton = 0
-# Check the current version and the newest version
 
+# Main Install Function
 def Install():
     RemoveOldFiles()
     DownloadNewVersion()
@@ -50,15 +60,16 @@ def CheckVersion():
     global installButton
     global warningLabel
     global changeLog
+    global changeLogButton
     try:
         # Get local version
         localVersion = open(os.path.dirname(os.path.abspath(__file__)) +"\\"+ dest + "\\version.txt", "r").read().split(",")[0] + " from " + open(os.path.dirname(os.path.abspath(__file__)) +"\\"+ dest + "\\version.txt", "r").read().split(",")[1]
         # Check the newest version
-        newestVersionFile, headers = urllib.request.urlretrieve("https://raw.githubusercontent.com/Tumppi066/Euro-Truck-Simulator-2-Lane-Assist/main/version.txt")
+        newestVersionFile, headers = urllib.request.urlretrieve("https://raw.githubusercontent.com/Tumppi066/Euro-Truck-Simulator-2-Lane-Assist/{}/version.txt".format(branch))
         newestVersion = open(newestVersionFile, "r").read().split(",")[0] + " from " + open(newestVersionFile, "r").read().split(",")[1]
         os.remove(newestVersionFile)
         # Check the change log        
-        changeLogFile, headers = urllib.request.urlretrieve("https://raw.githubusercontent.com/Tumppi066/Euro-Truck-Simulator-2-Lane-Assist/main/changelog.txt")
+        changeLogFile, headers = urllib.request.urlretrieve("https://raw.githubusercontent.com/Tumppi066/Euro-Truck-Simulator-2-Lane-Assist/{}/changelog.txt".format(branch))
         changeLog = open(changeLogFile, "r").read().split("Update")[1]
         os.remove(changeLogFile)
         currentLabel.set(("Current version: " + localVersion).replace("\n", ""))
@@ -84,17 +95,15 @@ def CheckVersion():
         pass
 
 
-# Make sure the user understands that the program will delete the old files
+
 def RemoveOldFiles():
     # Delete the old files
     shutil.rmtree(dest, ignore_errors=True)
-    print("Files removed")
-
-# Download percentage display
-# Credits to https://blog.shichao.io/2012/10/04/progress_speed_indicator_for_urlretrieve_in_python.html
+    print("Old version removed")
 
 
 def ShowChangeLog():
+    # Show the change log in a new window
     changeLogWindow = tk.Tk()
     changeLogWindow.title("Change log")
     changeLogWindow.geometry("600x600")
@@ -104,16 +113,22 @@ def ShowChangeLog():
 
 def DownloadNewVersion():
     # Download the newest version
-    name, headers = urllib.request.urlretrieve("https://github.com/Tumppi066/Euro-Truck-Simulator-2-Lane-Assist/archive/refs/heads/main.zip", "tempFile.zip")
-    print("Downloading please wait...")
+    try:
+        name, headers = urllib.request.urlretrieve("https://github.com/Tumppi066/Euro-Truck-Simulator-2-Lane-Assist/archive/refs/heads/{}.zip".format(branch), "tempFile.zip")
+        PrintGreen("Downloading new version please wait...")
 
-    # Unzip the new version
-    with zipfile.ZipFile(name, 'r') as zip_ref:
-        zip_ref.extractall("")
+        # Unzip the new version
+        with zipfile.ZipFile(name, 'r') as zip_ref:
+            zip_ref.extractall("")
 
-    # Remove the temporary zip
-    os.remove(name)
-    print("Download complete")
+        # Remove the temporary zip
+        os.remove(name)
+        PrintGreen("Download complete")
+    except Exception as e:
+        PrintRed("Error while downloading new version from github")
+        print(e)
+        exit()
+
 
 isMac = 0
 isGPU = 0
@@ -124,9 +139,8 @@ def AskForInformation():
     global isGPU
     global installWindow
     global model
-    # Install the requirements from the requirements.txt file
-    # This piece of code get's the current path and appends the newly downloaded file path to it.
-    # os.path.dirname(os.path.abspath(__file__)) +"\\"+ dest + "\\requirements.txt"
+
+    # Make the options window
     installWindow = tk.Tk()
     installWindow.title("Additional Installation Options...")
     installWindow.geometry("250x120")
@@ -139,34 +153,47 @@ def AskForInformation():
     gpu.pack()
     modelCheckButton = tk.Checkbutton(installWindow, variable=model, text="Open models in Web Browser?", font=("Calibri", 12), onvalue=1, offvalue=0)
     modelCheckButton.pack()
+    # Make a button that calls InstallRequirements() once clicked
     installButton = tk.Button(installWindow, text="Install", command=InstallRequirements, font=("Calibri", 12))
     installButton.pack()
 
 
 def InstallRequirements():
-    if(model.get() == 1):
-        webbrowser.open("https://github.com/Tumppi066/Euro-Truck-Simulator-2-Lane-Assist#lane-detection-models")
-    installWindow.destroy()
-    #path = "'" + os.path.dirname(os.path.abspath(__file__)) +"\\"+ dest + "\\requirements.txt'"
-    path = dest + "\\requirements.txt"
-    os.system("pip install -r " + path)
-    if isMac.get() == 1:
-        os.system("pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu")
-    elif isGPU.get() == 1:
-        webbrowser.open("https://developer.nvidia.com/cuda-downloads")
-        os.system("pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116")
-    else:
-        os.system("pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu")
-    print("Everything is installed")
+    try:
+        if(model.get() == 1):
+            webbrowser.open("https://github.com/Tumppi066/Euro-Truck-Simulator-2-Lane-Assist#lane-detection-models")
+        installWindow.destroy()
+        path = dest + "\\requirements.txt"
+        requirements = open(path, "r").readlines()
+        PrintGreen("Installing requirements...")
+        for line in range(len(requirements)):
+            PrintGreen("{} of {}".format(line + 1, len(requirements)))
+            line = requirements[line].replace("\n", "")
+            os.system("pip install " + line)
+        if isMac.get() == 1:
+            PrintGreen("Installing torch for mac...")
+            os.system("pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu")
+        elif isGPU.get() == 1:
+            PrintGreen("Installing torch for NVIDIA GPUs...")
+            webbrowser.open("https://developer.nvidia.com/cuda-downloads")
+            os.system("pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116")
+        else:
+            PrintGreen("Installing torch for CPUs...")
+            os.system("pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu")
+        PrintGreen("Everything is installed")
+    except Exception as e:
+        PrintRed("Error while installing requirements")
+        print(e)
+        exit()
 
 # Default TKinter window
 width = 250
 height = 300
 root = tk.Tk()
-root.title("ETS2 Lane Assist Updater V0.2")
+root.title("ETS2 Lane Assist Updater V0.3")
 root.geometry("%dx%d" % (width, height))
 root.configure(bg='#1c1c1c', padx=10, pady=10)
-AddLabel("ETS2 Lane Assist Updater V0.2", root, size=14)
+AddLabel("ETS2 Lane Assist Updater V0.3", root, size=14)
 currentLabel = AddLabel("Current version: " + "unknown", root, size=10)
 newestLabel = AddLabel("Newest version: " + "unknown", root, size=10)
 AddButton("Check versions", CheckVersion, root)
