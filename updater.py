733bf0d0cba06b7de451e2cf7cab9f2e73945683
Tumppi066 <tuomas.keidasto@gmail.com>
2022-12-11 21:52:04 +0200

    First pass of anaconda updater

--- updater.py
+++ updater.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import ctypes
 
 print("""
       __                   ___           _     __ 
@@ -15,6 +16,11 @@ print("""
                                          
 """)
 
+print("Before you start, make sure you have done the following:")
+print("> Installed anaconda from https://www.anaconda.com/products/distribution#Downloads")
+print("> Gave yourself the full permission to anaconda's folder (C:\\ProgramData\\anaconda3)")
+input("> Press Enter to continue...\n")
+
 def CreateAnacondaEnvironment():
     # Create a new environment with 
     # pytorch
@@ -29,27 +35,28 @@ def CreateAnacondaEnvironment():
         print("> Conda is installed!\n")
     except Exception as ex:
         print(ex)
-        print("> Not installed, install it here https://www.anaconda.com/...")
+        print("> Anaconda is not installed, install it here https://www.anaconda.com/products/distribution#Downloads ...")
         print("> If you have installed it, make sure it is added to PATH...")
         print("> And then run this script again...")
-        
+        input("> Press Enter to exit...")
         sys.exit(0)
 
     input("""
 The script will now create and configure a new anaconda environment named LaneAssist.
 This will take a while, so please be patient.
-> Press Enter to continue...""")
+> Press Enter to continue...\n""")
+
 
     # Run the following command in command prompt
-    print("\nCreating Anaconda Environment...")
+    print("Creating Anaconda Environment...")
     try:
-        os.system("conda create -n LaneAssist python=3.9 --yes")
+        os.system("conda create -n LaneAssist python=3.10 --yes")
         os.system("conda activate LaneAssist")
-        #os.system("conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia --yes")
-        #os.system("conda install tensorflow-gpu --yes")
-        #os.system("conda install -c conda-forge onnx --yes")
-        #os.system("conda install -c conda-forge onnxruntime-gpu --yes")
-        #os.system("conda install -c conda-forge cudnn --yes")
+        os.system("conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia --yes")
+        os.system("conda install -c conda-forge cudnn --yes")
+        os.system("conda install tensorflow-gpu --yes")
+        os.system("conda install -c conda-forge onnx --yes")
+        os.system("conda install -c conda-forge onnxruntime-gpu --yes")
     except Exception as ex:
         print("Error while creating Anaconda Environment...")
         print(ex)
