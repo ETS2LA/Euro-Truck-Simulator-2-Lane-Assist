2390d5c908373ecbb9e13a88c101d29ea120e97f
Tumppi066 <tuomas.keidasto@gmail.com>
2023-08-22 11:34:27 +0300

    Temporary fix for playsound

--- src/sounds.py
+++ src/sounds.py
@@ -1,12 +1,21 @@
-import playsound
 from src.logger import print
 from src.variables import PATH
 
+try:
+    import playsound
+    sounds = True
+except:
+    sounds = False
+    print("Could not import playsound, sounds will not be played.")
+    
 def PlaySound(sound):
     try:
         dir = PATH + sound
-        print("Playing sound: " + dir)
-        playsound.playsound(dir, block=False)
+        if sounds:
+            print("Playing sound: " + dir)
+            playsound.playsound(dir, block=False)
+        else:
+            print("Could not play sound: " + dir)
     except Exception as ex:
         print(ex.args)
         pass
