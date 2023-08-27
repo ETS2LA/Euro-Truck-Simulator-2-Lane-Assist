44a01b3c85ad7f259756a60ecc66bc801c97c06c
Tumppi066 <tuomas.keidasto@gmail.com>
2023-08-27 21:05:13 +0300

    Tweak some values with moving the visualization

--- plugins/Map/Visualize/visualize.py
+++ plugins/Map/Visualize/visualize.py
@@ -75,7 +75,7 @@ def VisualizePrefabs(data, img=None, zoom=2):
     # Get the roads in the current area
     areaItems = []
     areaItems += prefabItems.GetItemsInTileByCoordinates(x, y)
-    tileCoords = prefabItems.GetTileCoordinates(x, y)
+    tileCoords = roads.GetTileCoordinates(x, y)
     
     # Also get the roads in the surrounding tiles
     areaItems += prefabItems.GetItemsInTileByCoordinates(x + 1000, y)
@@ -96,27 +96,29 @@ def VisualizePrefabs(data, img=None, zoom=2):
     
     curveCount = 0
     for item in areaItems:
-        if item.Prefab.ValidRoad:
-            xy = prefabItems.GetLocalCoordinateInTile(item.X, item.Z, tileCoords[0], tileCoords[1])
-            x = xy[0] + 500 * (zoom - 1)
-            y = xy[1] + 500 * (zoom - 1)
-            cv2.circle(img, (int(x), int(y)), 5, (0, 255, 0), -1)
-            
-            filename = item.Prefab.FilePath.split("/")[-1]
-            cv2.putText(img, filename, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-            
-            for curve in item.Prefab.PrefabCurves:
-                curveCount += 1
-                startXY = prefabItems.GetLocalCoordinateInTile(curve.startX + item.X, curve.startZ + item.Z, tileCoords[0], tileCoords[1])
-                endXY = prefabItems.GetLocalCoordinateInTile(curve.endX + item.X, curve.endZ + item.Z, tileCoords[0], tileCoords[1])
-                startX = startXY[0] + 500 * (zoom - 1)
-                startY = startXY[1] + 500 * (zoom - 1)
-                endX = endXY[0] + 500 * (zoom - 1)
-                endY = endXY[1] + 500 * (zoom - 1)
-            
-                cv2.line(img, (int(startX), int(startY)), (int(endX), int(endY)), (255, 255, 255), 1 + (zoom - 1))
+        try:
+            if item.Prefab.ValidRoad:
+                xy = roads.GetLocalCoordinateInTile(item.X, item.Z, tileCoords[0], tileCoords[1])
+                x = xy[0] + 500 * (zoom - 1)
+                y = xy[1] + 500 * (zoom - 1)
+                cv2.circle(img, (int(x), int(y)), 5, (0, 255, 0), -1)
+                
+                filename = item.Prefab.FilePath.split("/")[-1]
+                cv2.putText(img, filename, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                
+                for curve in item.Prefab.PrefabCurves:
+                    curveCount += 1
+                    startXY = roads.GetLocalCoordinateInTile(curve.startX + item.X, curve.startZ + item.Z, tileCoords[0], tileCoords[1])
+                    endXY = roads.GetLocalCoordinateInTile(curve.endX + item.X, curve.endZ + item.Z, tileCoords[0], tileCoords[1])
+                    startX = startXY[0] + 500 * (zoom - 1)
+                    startY = startXY[1] + 500 * (zoom - 1)
+                    endX = endXY[0] + 500 * (zoom - 1)
+                    endY = endXY[1] + 500 * (zoom - 1)
+                
+                    cv2.line(img, (int(startX), int(startY)), (int(endX), int(endY)), (255, 255, 255), 1 + (zoom - 1))
+        except: pass
     
-    print(f"Prefabs visualized in {round(time.time() - startTime, 2)} seconds")
+    # print(f"Prefabs visualized in {round(time.time() - startTime, 2)} seconds")
     
     cv2.putText(img, f"Prefabs: {len(areaItems)}", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     cv2.putText(img, f"Curves: {curveCount}", (10, 140), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
