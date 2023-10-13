b6749c5a5c58b74063840c8a842a6437ef7342c5
Dylan B <110776467+DylDevs@users.noreply.github.com>
2023-10-13 11:03:27 -0400

    Make 3.12 check simpler

--- installer.py
+++ installer.py
@@ -111,19 +111,13 @@ if "OneDrive" in os.getcwd().lower():
     input()
     quit()
 
-# Check python version (must be > 3.9.x)
-if sys.version_info[0] != 3 or sys.version_info[1] < 9:
-    printRed("This app requires atleast python version 3.9.x to create the correct virtual environment.")
+# Check python version (must be > 3.9.x and < 3.12.x)
+if sys.version_info[0] != 3 or sys.version_info[1] < 9 or sys.version_info[1] > 11:
+    printRed("This app requires a Python version above 3.9.x and below 3.12.x to create the correct virtual environment.")
     printRed("Please install another version and run the installer again.")
     input()
     quit()
 
-# Check python version (Must be < 3.12.x)
-if sys.version_info[1] > 11:
-    printRed("This app requires a Python version below 3.12.x to create the correct virtual environment.")
-    printRed("Please install another version and run the installer again.")
-    input()
-    quit()
 
 # Check for a .bat file
 if not os.path.exists("installer.bat"):
