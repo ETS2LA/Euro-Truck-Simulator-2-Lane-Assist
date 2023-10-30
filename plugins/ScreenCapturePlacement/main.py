591919b1b539a46ec1a8019dae80ce4a3192934e
DylDev <110776467+DylDevs@users.noreply.github.com>
2023-10-29 21:02:28 -0400

    Fixed Detectionmethod not updating when opening

--- plugins/ScreenCapturePlacement/main.py
+++ plugins/ScreenCapturePlacement/main.py
@@ -29,8 +29,11 @@ import screeninfo
 global detectionmethod
 if "NavigationDetection" in settings.GetSettings("Plugins", "Enabled"):
     detectionmethod = "nav"
+elif "UFLDLaneDetection" in settings.GetSettings("Plugins", "Enabled"):
+    detectionmethod = "ufld"
 else:
-    detectionmethod = "other"
+    detectionmethod = "lstr"
+
 
 def GetTitlebarHeight():
     global titlebarHeight
@@ -174,18 +177,30 @@ class UI():
             helpers.MakeLabel(self.root, "Compatibility mode is for older versions of Windows\nwhere the transparent background doesn't work.\nCreate the window again after toggling!", 6,0, font=("Roboto", 8), padx=30, pady=10)
             helpers.MakeCheckButton(self.root, "Compatibility mode", "ScreenCapturePlacement", "compatibilityMode", 7,0, width=30, callback=lambda: LostFocus())
             
-            def checknav():
+            global detectionmethod
+            if "NavigationDetection" in settings.GetSettings("Plugins", "Enabled"):
+                detectionmethod = "nav"
+            elif "UFLDLaneDetection" in settings.GetSettings("Plugins", "Enabled"):
+                detectionmethod = "ufld"
+            else:
+                detectionmethod = "lstr"
+            def checkdetection():
                 if detectionmethod == "nav":
                     mainUI.switchSelectedPlugin("plugins.NavDetectionSetup.main")
+                elif detectionmethod == "ufld":
+                    mainUI.switchSelectedPlugin("plugins.UFLDInstaller.main")
                 else:
                     variables.RELOAD = True
-            
+
             if detectionmethod == "nav":
                 buttontext = "Navigation Detection Setup"
-                helpers.MakeButton(self.root, buttontext, checknav, 8,0, padx=10, pady=10, width=30)
+                helpers.MakeButton(self.root, buttontext, checkdetection, 8,0, padx=10, pady=10, width=30)
+            elif detectionmethod == "ufld":
+                buttontext = "UFLD Installer"
+                helpers.MakeButton(self.root, buttontext, checkdetection, 8,0, padx=10, pady=10, width=30)
             else:
                 buttontext = "Finish"
-                helpers.MakeButton(self.root, buttontext, checknav, 8,0, padx=10, pady=10, width=15)
+                helpers.MakeButton(self.root, buttontext, checkdetection, 8,0, padx=10, pady=10, width=15)
             
             self.root.pack(anchor="center", expand=False)
             self.root.update()
