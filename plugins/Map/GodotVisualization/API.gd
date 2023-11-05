f32104de9d1c60907f15d6cd18dca420eb622058
Tumppi066 <tuomas.keidasto@gmail.com>
2023-11-05 21:04:42 +0200

    Draw the roads with Godot. Still problems with triangles

--- plugins/Map/GodotVisualization/API.gd
+++ plugins/Map/GodotVisualization/API.gd
@@ -22,6 +22,8 @@ func RequestComplete(result, response_code, headers, body):
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
 	var roads = response["GPS"]["roads"]
+	var x = response["api"]["truckPlacement"]["coordinateX"]
+	var y = -response["api"]["truckPlacement"]["coordinateZ"]
 
 	# Road Data Includes
 	# Uid = 0
@@ -46,8 +48,8 @@ func RequestComplete(result, response_code, headers, body):
 		var roadPoints = []
 		for point in road["Points"]:
 			var pointX = point["X"]
-			var pointY = point["Z"]
-			roadPoints.append([pointX, pointY])
+			var pointZ = point["Z"]
+			roadPoints.append([pointX, pointZ])
 			
 		globalRoads.append(roadPoints)
 		
@@ -55,11 +57,63 @@ func RequestComplete(result, response_code, headers, body):
 	# Now instantiate the points
 	print("Instantiating circles")
 	for road in globalRoads:
+		var roadPoints = []
 		for point in road:
-			var sphere = CSGSphere3D.new()
-			add_child(sphere)
-			sphere.position = Vector3(point[0], point[1], 0)
-	print("Done")
+			roadPoints.append(Vector3(point[0], 0, point[1]))
+			# var sphere = CSGSphere3D.new()
+			# sphere.radius = 5
+			# add_child(sphere)
+			# sphere.position = Vector3(point[0], 0, point[1])
 	
-	camera.position = Vector3(globalRoads[-1][0][0], globalRoads[-1][0][1], 0)
-			
+		# Now create the road mesh and instantiate the road
+		var roadMesh = ArrayMesh.new()
+		var roadSurface = SurfaceTool.new()
+		var roadWidth = 4.5
+		roadSurface.begin(Mesh.PRIMITIVE_LINE_STRIP)
+		for point in roadPoints:
+			roadSurface.add_vertex(point)
+			# Add the same point but with a width
+			roadSurface.add_vertex(Vector3(point.x + roadWidth/2, point.y, point.z))
+			roadSurface.add_vertex(Vector3(point.x - roadWidth/2, point.y, point.z))
+
+		roadSurface.commit(roadMesh)
+		
+		var roadInstance = MeshInstance3D.new()
+		roadInstance.mesh = roadMesh
+
+		# Give it a material
+		var roadMaterial = StandardMaterial3D.new()
+		roadMaterial.albedo_color = Color(0.8, 0.8, 0.8)
+		roadMaterial.emission_enabled = true
+		roadMaterial.emission_energy = 0.5
+		roadInstance.material_override = roadMaterial
+
+		add_child(roadInstance)
+	
+
+	print("Done")
+	var roadCount = len(globalRoads)
+	print(len(globalRoads))
+	
+	camera.position = Vector3(x, 100, y)
+	print(camera.position)
+	
+	var closestDistance = 1000000
+	var closestPoint = []
+
+	for road in globalRoads:
+		for point in road:
+			# Get the distance to the camera
+			var distance = camera.position.distance_to(Vector3(point[0], point[1], 0))
+			print(distance)
+			if distance < closestDistance:
+				closestDistance = distance
+				closestPoint = point
+
+	# Move the camera to the last point
+	camera.position = Vector3(closestPoint[0], 100, closestPoint[1])
+
+	# Turn the camera to face the closest point
+	camera.look_at(Vector3(closestPoint[0], 0, closestPoint[1]), Vector3(0, 0, 1))
+	# But lock the tilt
+	camera.rotation_degrees = Vector3(camera.rotation_degrees.x, camera.rotation_degrees.y, 0)
