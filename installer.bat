a0b78200f0f6bfcac4483d8cf85c0ab2ae450c5a
Lenley <72272909+Cloud-121@users.noreply.github.com>
2023-12-30 15:44:50 -0600

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,24 +1,25 @@
 winget --version >nul 2>&1 || (
-    echo WARNING, You do not have winget avialble on your system, This is most lickly because your on a windows version older then 2004. Please update your windows install and try again.
+    color 4 & echo WARNING, You do not have winget avialble on your system, This is most lickly because your on a windows version older then 2004. Please update your windows install and try again.
     pause
     exit 0
 )
 
 git --version >nul 2>&1 || (
-    echo Installing git, Please read and accept the windows smart screen prompt
+    color 6 & echo Installing git, Please read and accept the windows smart screen prompt
     winget install Git.Git
     echo git is now installed
 )
 
 python --version >nul 2>&1 || (
-    echo Installing python, Please read and accept the windows smart screen prompt
+    color 6 & echo Installing python, Please read and accept the windows smart screen prompt
     winget install -e --id Python.Python.3.11
     echo Python is now installed
 )
+:start
 
 if exist installer.py (
     python --version >nul 2>&1 || (
-        echo Please rerun installer.bat
+        color 2 & echo Successfully install all requirements please re run installer.bat
         pause
         exit 0
     )
@@ -26,13 +27,15 @@ if exist installer.py (
     
 ) else (
     git --version >nul 2>&1 || (
-        echo Please rerun installer.bat
+        color 2 & echo Successfully install all requirements please re run installer.bat
         pause
         exit 0
     )
     git clone -b installer https://github.com/Tumppi066/Euro-Truck-Simulator-2-Lane-Assist.git
     xcopy Euro-Truck-Simulator-2-Lane-Assist\* . /E /H /C /Y
     rmdir /S /Q Euro-Truck-Simulator-2-Lane-Assist
+    color 2 & echo Successfully installed installer, Please re run installer.bat
+    pause
     
 )
 
