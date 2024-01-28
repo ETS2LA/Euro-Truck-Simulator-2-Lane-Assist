53171ed2b9f0ea62225a22eb7059ea37f814f069
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-01-28 23:42:46 +0200

    Fix installers, build docs

--- plugins/ExternalAPI/install.py
+++ plugins/ExternalAPI/install.py
@@ -13,10 +13,10 @@ def install():
         timer = time.time()
         try:
                 process = subprocess.Popen(["pip", "install", "-q", "-q", requirement])
-            except:
-                os.system("whoami")
-                print("Ignore the above text, for some reason the pip command failed, and I don't know why, but running whoami seems to fix it.\nPlease... if you know why this happens, do tell me.")
-                process = subprocess.Popen(["pip", "install", "-q", "-q", requirement])
+        except:
+            os.system("whoami")
+            print("Ignore the above text, for some reason the pip command failed, and I don't know why, but running whoami seems to fix it.\nPlease... if you know why this happens, do tell me.")
+            process = subprocess.Popen(["pip", "install", "-q", "-q", requirement])
         while process.poll() is None:
             if time.time() - timer > 0.2:
                 spinner.next()
