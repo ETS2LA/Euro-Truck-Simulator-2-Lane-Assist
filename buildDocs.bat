9b3ef9686508780d2e50393c019fb32b29613407
Tumppi066 <tuomas.keidasto@gmail.com>
2024-01-29 19:45:45 +0200

    Move docs to the corrent location

--- buildDocs.bat
+++ buildDocs.bat
@@ -5,3 +5,7 @@ echo The docs will be built in ./docs
 pause
 
 python -m pdoc --template-dir pdoc --force -o docs --html .
+
+echo Copying the Euro Truck Simulator 2 Lane Assist docs to the docs folder
+xcopy /E /Y docs\Euro-Truck-Simulator-2-Lane-Assist\* docs 
+rmdir /S /Q docs\Euro-Truck-Simulator-2-Lane-Assist
