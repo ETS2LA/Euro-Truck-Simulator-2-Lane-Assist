e4abfc2a236a29f5c6cb98f5950fca68f45c06a1
DylDev <dylanb92010@gmail.com>
2024-03-19 19:22:29 -0400

    Remove light theme things from theme switcher

--- plugins/ThemeSelector/main.py
+++ plugins/ThemeSelector/main.py
@@ -25,20 +25,10 @@ import src.settings as settings
 import os
 
 
-# Will switch the application theme
-# theme = "SunValley", "Forest", "Azure"
 reloads = 0
 def ChangeTheme(theme, root, changedColor=False):
     global reloads
-    
-    if theme != "SunValley" and theme != "Azure":
-        ChangeThemeCustom(fr"themes\{theme}\theme.tcl", root, "forest", name=theme, titlebar="0x313131") # Support custom themes from mainUI
-        return
-    
-    else:
-        helpers.ShowInfo("The old default themes have been deprecated, please use / create a new theme for yourself to use.\nFor now we have selected the default ForestRed theme.")
-        settings.CreateSettings("User Interface", "ColorTheme", "ForestRed")
-        ChangeThemeCustom(fr"themes\ForestRed\theme.tcl", root, "forest", name="ForestRed", titlebar="0x313131") # Support custom themes from mainUI
+    ChangeThemeCustom(fr"themes\{theme}\theme.tcl", root, "forest", name=theme, titlebar="0x313131")
     
 def ChangeThemeCustom(themePath, root, base, name="Custom", titlebar="0x313131"):
     global reloads
@@ -75,27 +65,14 @@ def ColorTitleBar(root, override=False):
     theme = settings.GetSettings("User Interface", "ColorTheme")
     
     # Needs to be int values so windows can understand them
-    # For dark themes it should be #2d2d2d and for light themes #f0f0f0
+    # It should be #2d2d2d
     # These have to then be converted to windows compatible int values
     if override == False:
-        darkTitlebarColors = {
-            "SunValley": 0x1c1c1c,
-            "Forest": 0x313131,
-            "Azure": 0x333333
-        }
-        lightTitlebarColors = {
-            "SunValley": 0xfafafa,
-            "Forest": 0xffffff,
-            "Azure": 0xffffff
-        }
-        
         # Change the titlebar color
         from ctypes import windll, c_int, byref, sizeof
         HWND = windll.user32.GetParent(mainUI.root.winfo_id())
-        try:
-            returnCode = windll.dwmapi.DwmSetWindowAttribute(HWND, 35, byref(c_int(lightTitlebarColors[theme]) if themeType == "light" else c_int(darkTitlebarColors[theme])), sizeof(c_int))
-        except:
-            returnCode = windll.dwmapi.DwmSetWindowAttribute(HWND, 35, byref(c_int(0x313131)), sizeof(c_int))
+
+        returnCode = windll.dwmapi.DwmSetWindowAttribute(HWND, 35, byref(c_int(0x313131)), sizeof(c_int))
         if settings.GetSettings("Dev", "print_ui_events", False) == True:
             print(f"Titlebar color change return code: {returnCode}")
     else:
@@ -105,14 +82,8 @@ def ColorTitleBar(root, override=False):
         from ctypes import windll, c_int, byref, sizeof
         HWND = windll.user32.GetParent(mainUI.root.winfo_id())
         returnCode = windll.dwmapi.DwmSetWindowAttribute(HWND, 35, byref(c_int(override)), sizeof(c_int))
-        if settings.GetSettings("Dev", "print_ui_events", False) == True:
-            print(f"Titlebar color change return code: {returnCode}")
-    
 
 def SwitchThemeType():
-    global themeType
-    print(themeType)
-    themeType = "dark" if themeType == "light" else "light"
     ChangeTheme(settings.GetSettings("User Interface", "ColorTheme"), mainUI.root, changedColor=True)
-    settings.CreateSettings("User Interface", "Theme", themeType)
-    print("Switched theme type to " + themeType)
+    settings.CreateSettings("User Interface", "Theme", "dark")
+    print("Switched theme type to dark")
