a80a751d39087925849b438fe67fa4c9c62e7307
DylDev <dylanb92010@gmail.com>
2024-03-24 20:45:34 -0400

    Prevent Error Spamming

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 """This file serves as the overseer to ETS2LA. It allows the app to restart itself without user input."""
 if __name__ == "__main__":
+    error = None
     # Import ETS2LA.core will import and run the app. Do that repeatedly in case of a crash.
     while True:
         try:
             import ETS2LA.core
+            if error is not None:
+                print("ETS2LA has restarted successfully.")
+                error = None
+            else:
+                print("ETS2LA has started successfully.")
         except Exception as e:
             if Exception == SystemExit or Exception == KeyboardInterrupt:
                 print("ETS2LA has been stopped.")
                 break
+
+            import traceback
+            if traceback.format_exc() == error:
+                print("ETS2LA has crashed with the same error. Send the above traceback to the developers.")
+                input("Press enter to exit...")
+                break
             
             print(f"ETS2LA has crashed with the following error:")
-            import traceback
             traceback.print_exc()
-            print("Restarting...")
+            error = traceback.format_exc()
+            print("\nAttempting to restart...")
             continue
