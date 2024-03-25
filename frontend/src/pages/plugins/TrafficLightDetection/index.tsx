2d14c864725ff495ee7d19f40f947b5159b43aad
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-03-25 23:25:05 +0100

    tld ui

--- frontend/src/pages/plugins/TrafficLightDetection/index.tsx
+++ frontend/src/pages/plugins/TrafficLightDetection/index.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function TrafficLightDetection() {
     return (
-        <div style={{ position: 'absolute', top: '50px', left: '50px' }}>
+        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
             <Button>Test</Button>
         </div>
     )
