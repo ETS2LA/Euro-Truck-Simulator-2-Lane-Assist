6acf5222c3689527af8c9e999444560085a94044
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-03-25 23:20:03 +0100

    updated the gitignore and added test ui for tld

---
+++ frontend/src/pages/plugins/TrafficLightDetection/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function TrafficLightDetection() {
+    return (
+        <div style={{ position: 'absolute', top: '50px', left: '50px' }}>
+            <Button>Test</Button>
+        </div>
+    )
+}
