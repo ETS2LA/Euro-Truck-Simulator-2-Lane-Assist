1ddf06c74050bbb6ef77fd1cab42ab5498c8bd43
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-03-26 00:32:17 +0100

    updated tld ui

--- frontend/src/pages/plugins/TrafficLightDetection/index.tsx
+++ frontend/src/pages/plugins/TrafficLightDetection/index.tsx
@@ -6,12 +6,16 @@ import { Card } from "@/components/ui/card"
 export default function TrafficLightDetection() {
     return (
         <Card className="flex flex-col content-center text-center pt-10 space-y-5 pb-0 h-[calc(100vh-75px)] overflow-auto">
-            <div className="flex space-x-3 w-10">
+            <div style={{ position: 'absolute', top: '75px', left: '25px' }}>
                 <Switch />
-                <p className="w-[100px]">Something...</p>
+                <label style={{ position: 'absolute', top: '-2px', left: '35px', width: '100px' }}>its a switch</label>
             </div>
-            <Button variant="default" className="w-[100px]">Something...</Button>
-            <Input className="w-[100px]" placeholder="Something..." />
+            <div style={{ position: 'absolute', top: '90px', left: '25px' }}>
+                <Button>Button</Button>
+            </div>
+            <div style={{ position: 'absolute', top: '130px', left: '25px' }}>
+                <Input placeholder="Input" />
+            </div>
         </Card>
     )
 }
