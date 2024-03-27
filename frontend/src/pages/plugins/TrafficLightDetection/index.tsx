71a84d57375f0c089d5f1b7c1f33d4c230d03ebd
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-03-28 00:12:01 +0100

    tld ui update

--- frontend/src/pages/plugins/TrafficLightDetection/index.tsx
+++ frontend/src/pages/plugins/TrafficLightDetection/index.tsx
@@ -24,12 +24,20 @@ export default function TrafficLightDetection({ ip }: { ip: string }) {
     const [TestCheckbox, setTestCheckbox] = useState<boolean | undefined>(undefined);
     const [TestInput, setTestInput] = useState<number | undefined>(undefined);
 
+    const [YellowLightDetection, setYellowLightDetection] = useState<boolean | undefined>(undefined);
+    const [PerformanceMode, setPerformanceMode] = useState<boolean | undefined>(undefined);
+    const [AdvancedSettings, setAdvancedSettings] = useState<boolean | undefined>(undefined);
+
     useEffect(() => {
         if (data) {
 
             if (data.TestSwitch !== undefined) { setTestSwitch(data.TestSwitch); } else { setTestSwitch(false); }
             if (data.TestCheckbox !== undefined) { setTestCheckbox(data.TestCheckbox); } else { setTestCheckbox(false); }
             if (data.TestInput !== undefined) { setTestInput(data.TestInput); } else { setTestInput(0); }
+
+            if (data.YellowLightDetection !== undefined) { setYellowLightDetection(data.YellowLightDetection); } else { setYellowLightDetection(false); }
+            if (data.PerformanceMode !== undefined) { setPerformanceMode(data.PerformanceMode); } else { setPerformanceMode(true); }
+            if (data.AdvancedSettings !== undefined) { setAdvancedSettings(data.AdvancedSettings); } else { setAdvancedSettings(false); }
             
         }
     }, [data]);
@@ -67,6 +75,36 @@ export default function TrafficLightDetection({ ip }: { ip: string }) {
         setTestInput(newTestInput);
     };
 
+    const UpdateYellowLightDetection = async () => {
+        let newYellowLightDetection = !YellowLightDetection;
+        toast.promise(SetSettingByKey("TrafficLightDetection", "YellowLightDetection", newYellowLightDetection, ip), {
+            loading: "Saving...",
+            success: "Set value to " + newYellowLightDetection,
+            error: "Failed to save"
+        });
+        setYellowLightDetection(newYellowLightDetection);
+    };
+
+    const UpdatePerformanceMode = async () => {
+        let newPerformanceMode = !PerformanceMode;
+        toast.promise(SetSettingByKey("TrafficLightDetection", "PerformanceMode", newPerformanceMode, ip), {
+            loading: "Saving...",
+            success: "Set value to " + newPerformanceMode,
+            error: "Failed to save"
+        });
+        setPerformanceMode(newPerformanceMode);
+    };
+
+    const UpdateAdvancedSettings = async () => {
+        let newAdvancedSettings = !AdvancedSettings;
+        toast.promise(SetSettingByKey("TrafficLightDetection", "AdvancedSettings", newAdvancedSettings, ip), {
+            loading: "Saving...",
+            success: "Set value to " + newAdvancedSettings,
+            error: "Failed to save"
+        });
+        setAdvancedSettings(newAdvancedSettings);
+    };
+
     return (
         <Card className="flex flex-col content-center text-center pt-10 space-y-5 pb-0 h-[calc(100vh-75px)] overflow-auto">
 
@@ -95,85 +133,128 @@ export default function TrafficLightDetection({ ip }: { ip: string }) {
                 </TabsList>
                 <TabsContent value="test">
 
-                <div style={{ position: 'absolute', left: '-194px', right: '12pt' }}>
+                    <div style={{ position: 'absolute', left: '-194px', right: '12pt' }}>
 
-                    <div style={{ position: 'absolute', top: '15px' }}>
-                        <Button>Primary Button</Button>
-                        <Button variant='secondary' style={{ marginLeft: '10px' }}>Secondary Button</Button>
-                        <Button variant='destructive' style={{ marginLeft: '10px' }}>Destructive Button</Button>
-                        <Button variant="outline" style={{ marginLeft: '10px' }}>Outline Button</Button>
-                        <Button variant="ghost" style={{ marginLeft: '10px' }}>Ghost Button</Button>
-                        <Button variant="link">Link Button</Button>
+                        <div style={{ position: 'absolute', top: '15px' }}>
+                            <Button>Primary Button</Button>
+                            <Button variant='secondary' style={{ marginLeft: '10px' }}>Secondary Button</Button>
+                            <Button variant='destructive' style={{ marginLeft: '10px' }}>Destructive Button</Button>
+                            <Button variant="outline" style={{ marginLeft: '10px' }}>Outline Button</Button>
+                            <Button variant="ghost" style={{ marginLeft: '10px' }}>Ghost Button</Button>
+                            <Button variant="link">Link Button</Button>
+                        </div>
+
+                        {TestSwitch !== undefined && (
+                        <div style={{ position: 'absolute', top: '62px' }}>
+                            <Switch id="switch" checked={TestSwitch} onCheckedChange={UpdateTestSwitch} />
+                            <Label htmlFor="switch" style={{ position: 'relative', top: '-2px', left: '5px', width: '800px', textAlign: 'left' }}>This is a label which is connected to the switch. (you can click on the label to change the switch state)</Label>
+                        </div>
+                        )}
+
+                        {TestCheckbox !== undefined && (
+                        <div style={{ position: 'absolute', top: '90px' }}>
+                            <Checkbox id="checkbox" checked={TestCheckbox} onCheckedChange={UpdateTestCheckbox} />
+                            <Label htmlFor="checkbox" style={{ position: 'relative', top: '-2px', left: '5px', width: '800px', textAlign: 'left' }}>This is a label which is connected to the checkbox. (you can click on the label to change the checkbox state)</Label>
+                        </div>
+                        )}
+
+                        <div style={{ position: 'absolute', top: '120px', width: '200px' }}>
+                            <RadioGroup defaultValue="option1">
+                                <div className="flex items-center space-x-2">
+                                    <RadioGroupItem value="option1" id="r1"/>
+                                    <Label htmlFor="r1">Option 1</Label>
+                                </div>
+                                <div className="flex items-center space-x-2">
+                                    <RadioGroupItem value="option2" id="r2"/>
+                                    <Label htmlFor="r2">Option 2</Label>
+                                </div>
+                                <div className="flex items-center space-x-2">
+                                    <RadioGroupItem value="option3" id="r3"/>
+                                    <Label htmlFor="r3">Option 3</Label>
+                                </div>
+                            </RadioGroup>
+                        </div>
+
+                        <div style={{ position: 'absolute', top: '200px', width: '200px' }}>
+                            <Slider defaultValue={[50]} min={0} max={100} step={1} />
+                        </div>
+
+                        {TestInput !== undefined && (
+                        <div style={{ position: 'absolute', top: '218px', width: '200px' }}>
+                            <Input placeholder={TestInput as unknown as string} onChangeCapture={(e) => UpdateTestInput(e)} />
+                        </div>
+                        )}
+
+                        <div style={{ position: 'absolute', top: '260px', width: '200px' }}>
+                            <Select>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select a YOLO model" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                    <SelectLabel>Select a YOLO model</SelectLabel>
+                                    <SelectItem value="yolov5n">YOLOv5n</SelectItem>
+                                    <SelectItem value="yolov5s">YOLOv5s</SelectItem>
+                                    <SelectItem value="yolov5m">YOLOv5m</SelectItem>
+                                    <SelectItem value="yolov5l">YOLOv5l</SelectItem>
+                                    <SelectItem value="yolov5x">YOLOv5x</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+
+                        <div style={{ position: 'absolute', top: '285px' }}>
+                            <div style={{ position: 'relative', top: '22px', left: '0px', filter: 'invert(1)' }} className="h-6 w-6 animate-spin rounded-full border-4 border-t-transparent border-currentColor-500"></div>
+                            <Label style={{ position: 'relative', top: '-2px', left: '32px', width: '800px', textAlign: 'left' }}>Loading...</Label>
+                        </div>
+
                     </div>
-
-                    {TestSwitch !== undefined && (
-                    <div style={{ position: 'absolute', top: '62px' }}>
-                        <Switch id="switch" checked={TestSwitch} onCheckedChange={UpdateTestSwitch} />
-                        <Label htmlFor="switch" style={{ position: 'relative', top: '-2px', left: '5px', width: '800px', textAlign: 'left' }}>This is a label which is connected to the switch. (you can click on the label to change the switch state)</Label>
-                    </div>
-                    )}
-
-                    {TestCheckbox !== undefined && (
-                    <div style={{ position: 'absolute', top: '90px' }}>
-                        <Checkbox id="checkbox" checked={TestCheckbox} onCheckedChange={UpdateTestCheckbox} />
-                        <Label htmlFor="checkbox" style={{ position: 'relative', top: '-2px', left: '5px', width: '800px', textAlign: 'left' }}>This is a label which is connected to the checkbox. (you can click on the label to change the checkbox state)</Label>
-                    </div>
-                    )}
-
-                    <div style={{ position: 'absolute', top: '120px', width: '200px' }}>
-                        <RadioGroup defaultValue="option1">
-                            <div className="flex items-center space-x-2">
-                                <RadioGroupItem value="option1" id="r1"/>
-                                <Label htmlFor="r1">Option 1</Label>
-                            </div>
-                            <div className="flex items-center space-x-2">
-                                <RadioGroupItem value="option2" id="r2"/>
-                                <Label htmlFor="r2">Option 2</Label>
-                            </div>
-                            <div className="flex items-center space-x-2">
-                                <RadioGroupItem value="option3" id="r3"/>
-                                <Label htmlFor="r3">Option 3</Label>
-                            </div>
-                        </RadioGroup>
-                    </div>
-
-                    <div style={{ position: 'absolute', top: '200px', width: '200px' }}>
-                        <Slider defaultValue={[50]} min={0} max={100} step={1} />
-                    </div>
-
-                    {TestInput !== undefined && (
-                    <div style={{ position: 'absolute', top: '218px', width: '200px' }}>
-                        <Input placeholder={TestInput as unknown as string} onChangeCapture={(e) => UpdateTestInput(e)} />
-                    </div>
-                    )}
-
-                    <div style={{ position: 'absolute', top: '260px', width: '200px' }}>
-                        <Select>
-                            <SelectTrigger>
-                                <SelectValue placeholder="Select a YOLO model" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectGroup>
-                                <SelectLabel>Select a YOLO model</SelectLabel>
-                                <SelectItem value="yolov5n">YOLOv5n</SelectItem>
-                                <SelectItem value="yolov5s">YOLOv5s</SelectItem>
-                                <SelectItem value="yolov5m">YOLOv5m</SelectItem>
-                                <SelectItem value="yolov5l">YOLOv5l</SelectItem>
-                                <SelectItem value="yolov5x">YOLOv5x</SelectItem>
-                                </SelectGroup>
-                            </SelectContent>
-                        </Select>
-                    </div>
-
-                    <div style={{ position: 'absolute', top: '285px' }}>
-                        <div style={{ position: 'relative', top: '22px', left: '0px', filter: 'invert(1)' }} className="h-6 w-6 animate-spin rounded-full border-4 border-t-transparent border-currentColor-500"></div>
-                        <Label style={{ position: 'relative', top: '-2px', left: '32px', width: '800px', textAlign: 'left' }}>Loading...</Label>
-                    </div>
-
-                </div>
 
                 </TabsContent>
                 <TabsContent value="general">
+
+                    <div style={{ position: 'absolute', left: '-194px', right: '12pt' }}>
+
+                        {YellowLightDetection !== undefined && (
+                        <div style={{ position: 'absolute', top: '30px' }}>
+                            <Switch id="yellowlightdetection" checked={YellowLightDetection} onCheckedChange={UpdateYellowLightDetection} />
+                            <Label htmlFor="yellowlightdetection" style={{ position: 'absolute', top: '4px', left: '40px', width: 'calc(100vw - 80px)', fontWeight: 'bold', textAlign: 'left' }}>
+                                Yellow Light Detection (not recommended)
+                            </Label>
+                            <Label htmlFor="yellowlightdetection" style={{ position: 'absolute', top: '24px', left: '40px', width: 'calc(100vw - 80px)', textAlign: 'left' }}>
+                                If enabled, the traffic light detection tries to detect yellow traffic lights, but it is not recommended because it causes more wrong detected traffic lights.
+                            </Label>
+                        </div>
+                        )}
+
+                        {PerformanceMode !== undefined && (
+                        <div style={{ position: 'absolute', top: '90px' }}>
+                            <Switch id="performancemode" checked={PerformanceMode} onCheckedChange={UpdatePerformanceMode} />
+                            <Label htmlFor="performancemode" style={{ position: 'absolute', top: '4px', left: '40px', width: 'calc(100vw - 80px)', fontWeight: 'bold', textAlign: 'left' }}>
+                                Performance Mode (recommended)
+                            </Label>
+                            <Label htmlFor="performancemode" style={{ position: 'absolute', top: '24px', left: '40px', width: 'calc(100vw - 80px)', textAlign: 'left' }}>
+                                If enabled, the traffic light detection only detects red traffic lights, which increases performance, but does not reduce detection accuracy.
+                            </Label>
+                        </div>
+                        )}
+
+                        {AdvancedSettings !== undefined && (
+                        <div style={{ position: 'absolute', top: '150px' }}>
+                            <Switch id="advancedsettings" checked={AdvancedSettings} onCheckedChange={UpdateAdvancedSettings} />
+                            <Label htmlFor="advancedsettings" style={{ position: 'absolute', top: '4px', left: '40px', width: 'calc(100vw - 80px)', fontWeight: 'bold', textAlign: 'left' }}>
+                                Advanced Settings
+                            </Label>
+                            <Label htmlFor="advancedsettings" style={{ position: 'absolute', top: '24px', left: '40px', width: 'calc(100vw - 80px)', textAlign: 'left' }}>
+                                If enabled, the traffic light detection uses the settings you set in the Advanced tab. (could have a bad impact on performance)
+                            </Label>
+                            <Button variant={'destructive'} style={{ position: 'absolute', top: '44px', left: '40px' }}>
+                                Reset Advanced Settings to Default
+                            </Button>
+                        </div>
+                        )}
+
+                    </div>
 
                 </TabsContent>
                 <TabsContent value="screencapture">
