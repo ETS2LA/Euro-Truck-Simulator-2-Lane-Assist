853a4c8ada74c135bd947f631d7765f289b2fb37
DylDev <dylanb92010@gmail.com>
2024-04-23 06:41:13 -0400

    Remove error loop in main

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 """This file serves as the overseer to ETS2LA. It allows the app to restart itself without user input."""
 if __name__ == "__main__":
     import sys
+    import traceback
     import os
-    error = None
     # Import ETS2LA.core will import and run the app. Do that repeatedly in case of a crash.
     while True:
         try:
             import ETS2LA.core
             ETS2LA.core.run()
-            if error is not None:
-                print("ETS2LA has restarted successfully.")
-                error = None
-            else:
-                print("ETS2LA has started successfully.")
+            print("ETS2LA has started successfully!")
         except Exception as e:
             if e.args[0] == "exit":
                 if os.name == "nt":
@@ -29,24 +25,15 @@ if __name__ == "__main__":
                     os.system("pkill -f node")
                 print("\033[93m" + "ETS2LA is restarting..." + "\033[0m")
                 continue
-
-            import traceback
-            if traceback.format_exc() == error:
-                print("ETS2LA has crashed with the same error. Send the above traceback to the developers.")
-                if os.name == "nt":
-                    os.system("taskkill /F /IM node.exe")
-                else:
-                    os.system("pkill -f node")
-                input("Press enter to exit...")
-                print("\033[91m" + "ETS2LA has been closed." + "\033[0m")
-                sys.exit(0)
             
             print(f"ETS2LA has crashed with the following error:")
             traceback.print_exc()
             error = traceback.format_exc()
-            print("\nAttempting to restart...")
+            print("Send the above traceback to the developers.")
             if os.name == "nt":
-                    os.system("taskkill /F /IM node.exe")
+                os.system("taskkill /F /IM node.exe")
             else:
                 os.system("pkill -f node")
-            continue
+            print("\033[91m" + "ETS2LA has been closed." + "\033[0m")
+            input("Press enter to exit...")
+            sys.exit(0)
