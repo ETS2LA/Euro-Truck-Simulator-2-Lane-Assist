756133d9fea2b727c323f9ab58263e087fc68521
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-05-03 17:31:10 +0300

    fix fps counter, performance improvements

--- ETS2LA/plugins/DepthPrediction/main.py
+++ ETS2LA/plugins/DepthPrediction/main.py
@@ -11,6 +11,7 @@ import torch
 url = 'https://media.discordapp.net/attachments/1120721175337775124/1235728372395675730/3.png?ex=663615fa&is=6634c47a&hm=df2c1192545f9dddd12952888934879ec2f99d3319b69cea71b1b472c0f29248&=&format=webp&quality=lossless&width=885&height=498'
 image = Image.open(requests.get(url, stream=True).raw)
 runner:PluginRunner = None
+RESOLUTION_SCALE = 1/4
 
 def Initialize():
     global SI
@@ -34,10 +35,9 @@ def plugin():
     startTime = time.time()
     cropped, image = SC.run()
     # Lower the resolution to a 1/4
-    cropped = cv2.resize(cropped, (0,0), fx=0.5, fy=0.5)
+    cropped = cv2.resize(cropped, (0,0), fx=RESOLUTION_SCALE, fy=RESOLUTION_SCALE)
     # prepare image for the model
-    inputs = image_processor(images=cropped, return_tensors="pt", do_resize=False)
-    endTime = time.time()
+    inputs = image_processor(images=cropped, return_tensors="pt", do_resize=False, do_normalize=False)
     inputs = {name: tensor.to(device) for name, tensor in inputs.items()}
     # predict depth
     with torch.no_grad():
@@ -54,6 +54,7 @@ def plugin():
     output = prediction.squeeze().cpu().numpy()
     formatted = (output * 255 / np.max(output)).astype("uint8")
     depth = cv2.applyColorMap(formatted, cv2.COLORMAP_BONE)
+    endTime = time.time()
     # Add text
     cv2.putText(depth, f"FPS: {round(1/(endTime-startTime), 2)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
     SI.run(depth)
