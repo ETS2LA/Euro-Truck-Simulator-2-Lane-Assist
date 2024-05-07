91089afc3865e31fb04f32e9dcfd7dfafed0a1f4
DylDev <dylanb92010@gmail.com>
2024-05-06 20:53:29 -0400

    add serial.cpp and remove code.cpp

---
+++ ETS2LA/plugins/MicrocontrollerControl/Arduino/serial.cpp
@@ -0,0 +1,44 @@
+const byte numChars = 32;
+char receivedChars[numChars]; // an array to store the received data
+
+boolean newData = false;
+
+void setup() {
+ Serial.begin(9600);
+ Serial.println("<Arduino is ready>");
+}
+
+void loop() {
+ recvWithEndMarker();
+ showNewData();
+}
+
+void recvWithEndMarker() {
+ static byte ndx = 0;
+ char endMarker = '\n';
+ char rc;
+ 
+           while (Serial.available() > 0 && newData == false) {
+ rc = Serial.read();
+
+ if (rc != endMarker) {
+ receivedChars[ndx] = rc;
+ ndx++;
+ if (ndx >= numChars) {
+ ndx = numChars - 1;
+ }
+ }
+ else {
+ receivedChars[ndx] = '\0'; // terminate the string
+ ndx = 0;
+ newData = true;
+ }
+ }
+}
+
+void showNewData() {
+ if (newData == true) {
+ Serial.println(receivedChars);
+ newData = false;
+ }
+}
