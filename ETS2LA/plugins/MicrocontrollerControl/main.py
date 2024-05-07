fabf0375f6126c753d90e65eca39df26d7afe3de
DylDev <dylanb92010@gmail.com>
2024-05-06 20:44:02 -0400

    Add MicrocontrollerControl plugin (not working)

---
+++ ETS2LA/plugins/MicrocontrollerControl/main.py
@@ -0,0 +1,17 @@
+import ETS2LA.backend.settings as settings
+import ETS2LA.backend.controls as controls
+import ETS2LA.variables as variables
+from ETS2LA.plugins.runner import PluginRunner  
+import cv2
+import numpy as np
+
+runner:PluginRunner = None
+
+def SendCrashReport(): # REMOVE THIS LATER
+    return
+
+def Initialize():
+    pass
+
+def plugin():
+    pass
