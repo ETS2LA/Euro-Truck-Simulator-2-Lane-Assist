19273f55abd0e91f70c78461ee43f578e0687726
DylDev <dylanb92010@gmail.com>
2024-05-25 22:37:38 -0400

    Add a failsafe if server is down

--- frontend/src/pages/account.tsx
+++ frontend/src/pages/account.tsx
@@ -1,6 +1,36 @@
+import { title } from "process";
 import { token } from "./backend";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export async function CheckConnection() {
+    try {
+        let response = await fetch("https://api.tumppi066.fi/", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        if(response.ok) {
+            return true
+        }
+        throw new Error("Failed to connect to ETS2LA Backend Server")
+    } catch(error) {
+        toast.error("Failed to connect to ETS2LA Backend Server", 
+            {
+                description: "The server is most likely down, please use a guest account for now. We will notify you when the login system is back up and running!",  
+                duration: 10000
+            } 
+        )
+        return false
+    }
+}
 
 export async function CheckUsernameAvailability(username:string) {
+    if (await CheckConnection() === false) {
+        console.error("Failed to connect to backend at https://api.tumppi066.fi/!")
+        return false
+    }
     let response = await fetch("https://api.tumppi066.fi/account/username/check", {
         method: "POST",
         headers: {
@@ -16,6 +46,10 @@ export async function CheckUsernameAvailability(username:string) {
 }
 
 export async function Register(username:string, password:string) {
+    if (await CheckConnection() === false) {
+        console.error("Failed to connect to backend at https://api.tumppi066.fi/!")
+        return null
+    }
     let response = await fetch("https://api.tumppi066.fi/account/register", {
         method: "POST",
         headers: {
@@ -31,6 +65,14 @@ export async function Register(username:string, password:string) {
 }
 
 export async function Login(username:string, password:string) {
+    await CheckConnection().then((result) => {
+        console.log(result)
+        if (result === false) {
+            console.error("Failed to connect to backend at https://api.tumppi066.fi/!")
+            return null
+        }
+    })
+        
     let response = await fetch("https://api.tumppi066.fi/account/login", {
         method: "POST",
         headers: {
