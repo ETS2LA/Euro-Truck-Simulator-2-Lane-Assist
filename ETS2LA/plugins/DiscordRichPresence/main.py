ceaa71f55160438c8878e2c27ed0249477445f2f
DylDev <dylanb92010@gmail.com>
2024-06-05 15:47:23 -0400

    Add Working Discord RPC

--- ETS2LA/plugins/DiscordRichPresence/main.py
+++ ETS2LA/plugins/DiscordRichPresence/main.py
@@ -6,6 +6,7 @@ from ETS2LA.plugins.runner import PluginRunner
 
 from pypresence import Presence
 import time
+import sys
 
 runner:PluginRunner = None
 
@@ -16,22 +17,24 @@ def Initialize():
     global TruckSimAPI
     global rpc_update_interval
     global last_rpc_update
-    global client_id
+    global app_id
+    global RPC
     
     # Connect to Discord application
-    client_id = "1175725825493045268"
-    RPC = Presence(client_id)
+    app_id = "1172916310519320606"
+    RPC = Presence(app_id)
     RPC.connect()
     
     start = int(time.time())
     RPC.update(
-        state="ETS2LA | Menu",
+        state="ETS2LA | Initializing...",
         large_image="la4",
         large_text="Lane Assist",
         buttons=[{"label": "ETS2LA Github", "url": "https://github.com/ETS2LA/Euro-Truck-Simulator-2-Lane-Assist"}],
         start=start
     )
     logging.info("Discord Rich Presence has been started!")
+    sys.stdout.write("Discord Rich Presence has been started!\n")
 
     rpc_update_interval = 10
     last_rpc_update = 0
@@ -40,8 +43,49 @@ def Initialize():
 
 
 def plugin():
-    data = {}
-    data["api"] = TruckSimAPI.run()
+    global TruckSimAPI, rpc_update_interval, last_rpc_update, RPC
+
+    if int(time.time()) - last_rpc_update >= rpc_update_interval:
+        data = {}
+        data["api"] = TruckSimAPI.run()
+
+        connected = data["api"]["sdkActive"]
+        if connected == True:
+            paused = data["api"]["pause"]
+            game = data["api"]["scsValues"]["game"]
+            if game == "ETS2":
+                small_image = "ets2"
+                small_text = "Euro Truck Simulator 2"
+            elif game == "ATS":
+                small_image = "ats"
+                small_text = "American Truck Simulator"
+        else:
+            paused = True
+            game = "Game Disconnected"
+            small_image = None
+            small_text = None
 
 
-        
+        if paused == True:
+            game_state = "Game Paused"
+        else:
+            game_state = "Game Running"
+    
+        if connected == False:
+            RPC.update(
+                details="ETS2LA | Menu",
+                state="Game Disconnected",
+                large_image="la4",
+                large_text="Lane Assist",
+                buttons=[{"label": "ETS2LA Github", "url": "https://github.com/ETS2LA/Euro-Truck-Simulator-2-Lane-Assist"}]
+            )
+        else:
+            RPC.update(
+                details=f"ETS2LA | Enabled",
+                state=f"{game} - {game_state}",
+                large_image="la4",
+                large_text="Lane Assist",
+                small_image=small_image,
+                small_text=small_text,
+                buttons=[{"label": "ETS2LA Github", "url": "https://github.com/ETS2LA/Euro-Truck-Simulator-2-Lane-Assist"}]
+            )
