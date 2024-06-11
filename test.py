59e737113b0ff8ccecb60aa77df7094d8c7c8cd1
DylDev <dylanb92010@gmail.com>
2024-06-11 16:15:43 -0400

    Remove useless old/1.0 files

--- test.py
+++
@@ -1,7 +0,0 @@
-import git
-repo = git.Repo()
-current_hash = repo.head.object.hexsha
-o = repo.remotes.origin
-origin_hash = o.fetch()[0].commit.hexsha
-if current_hash != origin_hash:
-    print("Update available!")
