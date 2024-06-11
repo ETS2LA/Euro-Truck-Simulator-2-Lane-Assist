4331b968c554c394a2d8e701d43db8dba957043a
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-06-11 20:33:37 +0300

    add fps to vis

---
+++ godot/ETS2LA-Visualisation/label_fps.gd
@@ -0,0 +1,12 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var fps = Engine.get_frames_per_second()
+	self.text = "We are making this visualization... hold tight!\n(" + str(fps) + " fps)"
