c9942df6584afc0e5415ab9123ef077d8cd08074
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-06-26 17:01:51 +0200

    bug fix

--- src/pytorch.py
+++ src/pytorch.py
@@ -20,21 +20,21 @@ def CheckPyTorch():
         modules = result.stdout
         for module in modules.splitlines():
             if "torch " in module:
-                if "cu2" in module:
+                if "cu" in module:
                     print(RED + "'torch' is installed with CUDA, please check yourself why the app is not working." + NORMAL)
                     console.RestoreConsole()
                 else:
                     print(GREEN + "The app is working on a fix for the problem with 'torch', please don't close the app." + NORMAL)
                     threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch --upgrade", "torch")).start()
             elif "torchvision " in module:
-                if "cu2" in module:
+                if "cu" in module:
                     print(RED + "'torchvision' is installed with CUDA, please check yourself why the app is not working." + NORMAL)
                     console.RestoreConsole()
                 else:
                     print(GREEN + "The app is working on a fix for the problem with 'torchvision', please don't close the app." + NORMAL)
                     threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torchvision --upgrade", "torchvision")).start()
             elif "torchaudio " in module:
-                if "cu2d" in module:
+                if "cu" in module:
                     print(RED + "'torchaudio' is installed with CUDA, please check yourself why the app is not working." + NORMAL)
                     console.RestoreConsole()
                 else:
