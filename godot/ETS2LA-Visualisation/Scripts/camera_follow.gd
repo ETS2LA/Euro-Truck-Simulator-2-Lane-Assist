b41e4ee46c92dde39944e081563140c6a4619bab
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-07-10 13:54:19 +0300

    You can now move the camera in the visualisation

--- godot/ETS2LA-Visualisation/Scripts/camera_follow.gd
+++ godot/ETS2LA-Visualisation/Scripts/camera_follow.gd
@@ -5,6 +5,9 @@ extends Node3D
 @export var offset : Vector3
 @export var offsetRotation : Vector3
 @export var reverseOffsetRotation : Vector3
+@export var mouseRotationResetTime : int = 5000
+var mouseOffsetRotation = Vector3(0,0,0)
+var mouseRotationTime = Time.get_ticks_msec()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,26 +15,44 @@ func _ready() -> void:
 		print("WARNING: Camera has no target set!")
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var data = Sockets.data
+	var speed = 0
 	var followSpeed = 10
 	var rotationOffset = offsetRotation
 	if data != {}:
-		followSpeed = (float(data["speed"]) + 1) * 2
+		speed = float(data["speed"])
+		followSpeed = (speed + 1) * 2
 	if followSpeed < 0:
 		followSpeed = -followSpeed
 		rotationOffset = reverseOffsetRotation
 		
+	if Input.is_action_pressed("MouseHold"):
+		var mouseVelocity = Input.get_last_mouse_velocity()
+		mouseOffsetRotation.y -= mouseVelocity.x / DisplayServer.screen_get_size().x / 1.5 # Yaw
+		mouseOffsetRotation.x += mouseVelocity.y / DisplayServer.screen_get_size().y / 1.5 # Tilt
+		mouseRotationTime = Time.get_ticks_msec()
+	elif Time.get_ticks_msec() - mouseRotationTime > mouseRotationResetTime and speed * 3.6 > 20: # Reset back after time and if speed is over 20kph
+		mouseOffsetRotation = Vector3(0, 0, 0)
+		
 	# Lerp the position to the target position
 	self.position = self.position.lerp(target.position + offset, delta * followSpeed)
 	
-	# Lerp the rotation to the target rotation
+	# Yaw
 	var currentRotation = Quaternion(self.basis)
-	var targetRotation = Quaternion(Vector3(0,1,0), target.rotation.y + rotationOffset.y)
+	var targetRotation = Quaternion(Vector3(0,1,0), target.rotation.y + rotationOffset.y + mouseOffsetRotation.y)
 	var smoothrot = currentRotation.slerp(targetRotation, delta * 5)
 	
 	self.basis = Basis(smoothrot)
+	
+	# Tilt
+	#currentRotation = Quaternion(self.basis)
+	#targetRotation = Quaternion(Vector3(1,0,0), target.rotation.x + rotationOffset.x + mouseOffsetRotation.x)
+	#smoothrot = currentRotation.slerp(targetRotation, delta * 5)
+	
+	#self.basis = Basis(smoothrot)
+	
+	
 	
 	pass
