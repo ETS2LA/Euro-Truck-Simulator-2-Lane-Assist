e9bb5968c50145f60cce254820bbb3062a60bc48
DylDev <dylanb92010@gmail.com>
2024-07-18 11:10:17 -0400

    Couple more changes to ObjectDetection UI

--- frontend/src/pages/plugins/ObjectDetection/index.tsx
+++ frontend/src/pages/plugins/ObjectDetection/index.tsx
@@ -30,6 +30,7 @@ export default function ObjectDetection({ ip }: { ip: string }) {
         } else {
             setLaneChangingModelEnabled(false);
         }
+        console.log("Lane Changing Model Enabled was changed to: " + value + " This feature has not been added yet.");
     };
 
     // Vehicle Classification Model Enabled
@@ -40,6 +41,7 @@ export default function ObjectDetection({ ip }: { ip: string }) {
         } else {
             setVehicleClassificationModelEnabled(false);
         }
+        console.log("Vehicle Classification Model Enabled was changed to: " + value + " This feature has not been added yet.");
     };
 
     // Model Confidence Slider
@@ -47,7 +49,7 @@ export default function ObjectDetection({ ip }: { ip: string }) {
     const handleSliderChange = (value: number[]) => {
         if (value.length > 0) {
             setModelConfSlider(value[0]);
-            console.log("Model Confidence Slider Value was changed to: " + value[0] + "Nothing has been affected as this has not been added yet.");
+            console.log("Model Confidence Slider Value was changed to: " + value[0] + " This feature has not been added yet.");
         }
     };
 
@@ -80,19 +82,22 @@ export default function ObjectDetection({ ip }: { ip: string }) {
                     <TabsContent value="general">
                         <CardContent>
                             <div className="flex flex-col justify-start pt-2 self-start -ml-56 md:-ml-68">
-                                <div className="flex flex-row items-center text-left gap-2 pt-2">
-                                    <Switch id="grayscalewindow" checked={laneChangingModelEnabled} onCheckedChange={handleLaneChangingModelChange} />
-                                    <Label htmlFor="grayscalewindow">
-                                        <span className="font-bold">Lame Changing Assist Model</span><br />
-                                        If enabled, Vehicle Detection will use the Lame Changing Assist Model to check for vehicles before changing lanes.
-                                    </Label>
-                                </div>
-                                <div className="flex flex-row items-center text-left gap-2 pt-2">
-                                    <Switch id="grayscalewindow" checked={vehicleClassificationModelEnabled} onCheckedChange={handleVehicleClassificationModelChange} />
-                                    <Label htmlFor="grayscalewindow">
-                                        <span className="font-bold">Vehicle Classification Model</span><br />
-                                        If enabled, Vehicle Detection will use the Vehicle Classification Model to get details about vehicles for GoDot visualization.
-                                    </Label>
+                                <h4>Vehicle Detection Settings:</h4>
+                                <div className="flex flex-col pt-2">
+                                    <div className="flex flex-row items-center text-left gap-2 pt-2">
+                                        <Switch id="lanechanginemodelenabled" checked={laneChangingModelEnabled} onCheckedChange={handleLaneChangingModelChange} />
+                                        <Label htmlFor="lanechanginemodelenabled">
+                                            <span className="font-bold">Lame Changing Assist Model</span><br />
+                                            If enabled, Vehicle Detection will use the Lame Changing Assist Model to check for vehicles before changing lanes.
+                                        </Label>
+                                    </div>
+                                    <div className="flex flex-row items-center text-left gap-2 pt-2">
+                                        <Switch id="classificationmodelenabled" checked={vehicleClassificationModelEnabled} onCheckedChange={handleVehicleClassificationModelChange} />
+                                        <Label htmlFor="classificationmodelenabled">
+                                            <span className="font-bold">Vehicle Classification Model</span><br/>
+                                            If enabled, Vehicle Detection will use the Vehicle Classification Model to get details about vehicles for GoDot visualization.
+                                        </Label>
+                                    </div>
                                 </div>
                             </div>
                         </CardContent>
@@ -113,15 +118,18 @@ export default function ObjectDetection({ ip }: { ip: string }) {
                                         <TabsTrigger value="colors">Colors</TabsTrigger>
                                     </TabsList>
                                     <TabsContent value="detection">
-                                        <Badge variant={"destructive"}>
-                                            You shouldnt not mess with these settings unless you know what you are doing. 
-                                            These settings can be used to fine tune Vehicle Detection for your PC. 
-                                            If used inccorectly, it can have negative impacts. 
-                                            Normally, default settings are best.</Badge>
-                                            <div className="grid-flow-col">
-                                                <p>Model Confidence Limit</p>
-                                                <Slider defaultValue={[modelConfSlider]} max={1} step={0.01} onValueChange={handleSliderChange}></Slider>                         
-                                            </div>
+                                        <div className="pb-2">
+                                            <Badge variant={"destructive"}>
+                                                You shouldnt not mess with these settings unless you know what you are doing. 
+                                                These settings can be used to fine tune Vehicle Detection for your PC. 
+                                                If used inccorectly, it can have negative impacts. 
+                                                Normally, default settings are best.
+                                            </Badge>
+                                        </div>
+                                        <div className="grid-flow-col">
+                                            <p>Model Confidence Limit</p>
+                                            <Slider defaultValue={[modelConfSlider]} max={1} step={0.01} onValueChange={handleSliderChange}></Slider>                         
+                                        </div>
                                     </TabsContent>
                                     <TabsContent value="colors">
                                         <div className="grid grid-cols-3 grid-rows-3">
