225f8aad316b9b86404046cf8317672cc4e9b4fc
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-07-27 01:09:02 +0200

    fixed the controls.py popup for changing keybinds

--- ETS2LA/backend/controls.py
+++ ETS2LA/backend/controls.py
@@ -16,11 +16,13 @@ GetKeybindValue(name)
 
 from ETS2LA.frontend.immediate import send_sonner
 import ETS2LA.backend.settings as settings
+import ETS2LA.variables as variables
 from tktooltip import ToolTip
 from tkinter import ttk
 import tkinter as tk
 import threading
 import keyboard
+import colorsys
 import logging
 import pygame
 import math
@@ -176,16 +178,7 @@ def ChangeKeybind(name:str, callback=None):
     global save
     global ignore
     global currentbinding
-    
-    print("Changing keybind " + name)
-    UpdateKeybindsFromSettings() # Make sure we have the latest keybinds
-    # Make a new window to get the keybind on
-    window = tk.Tk()
-    window.title("Change keybind")
-    window.geometry(f"300x200")
-    window.resizable(False, False)
-    window.grab_set()
-    window.focus_set()
+
     keybindToChange = KEYBINDS[KEYBINDS.index(next((item for item in KEYBINDS if item["name"] == name), None))]
     if keybindToChange == None:
         print("Keybind not found")
@@ -194,81 +187,147 @@ def ChangeKeybind(name:str, callback=None):
         except:
             pass
         return
-    
+
+    try:
+        theme = settings.Get("global", "theme", "dark")
+        with open(f"{variables.PATH}frontend\\src\\pages\\globals.css", "r") as file:
+            content = file.read().split("\n")
+            for i, line in enumerate(content):
+                if i > 0:
+                    if content[i - 1].replace(" ", "").startswith(":root{" if theme == "light" else ".dark{") and line.replace(" ", "").startswith("--background"):
+                        line = str(line.split(":")[1]).replace(";", "")
+                        for i, char in enumerate(line):
+                            if char == " ":
+                                line = line[i+1:]
+                            else:
+                                break
+                        line = line.split(" ")
+                        line = [round(float(line[0])), round(float(line[1][:-1])), round(float(line[2][:-1]))]
+                        rgb_color = colorsys.hls_to_rgb(int(line[0])/360,int(line[2])/100,int(line[1])/100)
+                        hex_color = '#%02x%02x%02x'%(round(rgb_color[0]*255),round(rgb_color[1]*255),round(rgb_color[2]*255))
+                        BackgroundColor = hex_color
+    except:
+        try:
+            BackgroundColor = "#ffffff" if settings.Get("global", "theme", "dark") == "light" else "#09090b"
+        except:
+            BackgroundColor = "#09090b"
+
+    TextColor = '#%02x%02x%02x' % (255 - int(BackgroundColor[1:3], 16), 255 - int(BackgroundColor[3:5], 16), 255 - int(BackgroundColor[5:7], 16))
+    ActiveBackgroundColor = "#%02x%02x%02x" % (int(BackgroundColor[1:3], 16) + 20, int(BackgroundColor[3:5], 16) + 20, int(BackgroundColor[5:7], 16) + 20)
+
+    print("Changing keybind " + name)
+    UpdateKeybindsFromSettings() # Make sure we have the latest keybinds
+    # Make a new window to get the keybind on
+
+    if os.name == "nt":
+        import win32gui
+        hwnd = win32gui.FindWindow(None, f'ETS2LA - Tumppi066 & Contributors © {variables.YEAR}')
+        if hwnd != 0:
+            rect = win32gui.GetClientRect(hwnd)
+            tl = win32gui.ClientToScreen(hwnd, (rect[0], rect[1]))
+            br = win32gui.ClientToScreen(hwnd, (rect[2], rect[3]))
+            window = (tl[0], tl[1], br[0] - tl[0], br[1] - tl[1])
+            window_x = window[0] + window[2] / 2 - 150
+            window_y = window[1] + window[3] / 2 - (94 if keybindToChange["shouldBeAxis"] else 75)
+        else:
+            window_x = None
+            window_y = None
+    else:
+        window_x = None
+        window_y = None
+
+    window = tk.Tk()
+    import sv_ttk
+    sv_ttk.set_theme(theme, window)
+    window.title("Change keybind")
+    window.geometry("300x188" if keybindToChange["shouldBeAxis"] else "300x150")
+    if window_x != None and window_y != None:
+        window.geometry(f"+{int(window_x)}+{int(window_y)}")
+    window.resizable(False, False)
+    window.configure(bg=BackgroundColor)
+    window.grab_set()
+    window.focus_set()
+    window.update()
+
+    if os.name == "nt":
+        from ctypes import windll, byref, sizeof, c_int
+        windll.dwmapi.DwmSetWindowAttribute(windll.user32.GetParent(window.winfo_id()), 35, byref(c_int(int(int(BackgroundColor[5:7] + BackgroundColor[3:5] + BackgroundColor[1:3], 16)))), sizeof(c_int))
+        window.iconbitmap(default=f"{variables.PATH}frontend/src/assets/favicon.ico")
+
     # Try to grab focus
     try:
         window.attributes("-topmost", True)
     except:
         pass
-    
-    # Slider for the axis
+
     if keybindToChange["shouldBeAxis"]:
-        axisSlider = tk.Scale(window, from_=-1, to=1, orient="horizontal", length=200, resolution=0.01)
-        axisSlider.pack()
         # Make a label to show the current keybind
-        label = ttk.Label(window, text="Listening for input...\n(expecting an axis)")
+        label = ttk.Label(window, text="Listening for input...\n(expecting an axis)", foreground=TextColor, background=BackgroundColor)
         label.pack()
+        # Slider for the axis
+        axisSlider = tk.Scale(window, from_=-1, to=1, orient="horizontal", length=200, resolution=0.01, foreground=TextColor, background=BackgroundColor, activebackground=ActiveBackgroundColor, highlightthickness=0)
+        axisSlider.pack()
     else:
         # Make a label to show the current keybind
-        label = ttk.Label(window, text="Listening for input...\n(expecting a button)")
+        label = ttk.Label(window, text="Listening for input...\n(expecting a button)", foreground=TextColor, background=BackgroundColor)
         label.pack()
-    
-    ttk.Label(window, text="   ").pack()
-    
+
+    ttk.Label(window, text="", foreground=TextColor, background=BackgroundColor).pack()
+
     def IgnoreBind():
         global ignore
         ignore = True
         print("Ignoring next input")
-    
+
     def SaveBind():
         global save
         save = True
         window.destroy()
-    
+
     # Ignore button
     ignoreButton = ttk.Button(window, text="Ignore", command=lambda: IgnoreBind(), width=30)
     ignoreButton.pack()
-    
+
     # Save button
     saveButton = ttk.Button(window, text="Save", command=lambda: SaveBind(), width=30)
     saveButton.pack(pady=10)
-    
+
     # Get all devices from pygame
     pygame.init()
     pygame.joystick.init()
     pygame.event.pump()
     joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
-    
+
     import time
     time.sleep(0.2)
     pygame.event.pump()
-    
+
     # For all devices, save the default state
     defaultStates = []
     for joystick in joysticks:
         defaultStates.append({"name": joystick.get_name(), "buttons": [joystick.get_button(i) for i in range(joystick.get_numbuttons())], "axes": [joystick.get_axis(i) for i in range(joystick.get_numaxes())]})
-    
+
     save = False
     ignore = False
     currentbinding = None
-    
+
     def KeyboardEvent(event):
         global currentbinding
         if not keybindToChange["shouldBeAxis"]:
             if len(event.keysym) > 2:
                 return # Ignore special keys
-            label.config(text=f"Key: '{event.keysym}'")
+            label.config(text=f"Detected a keypress\nKey: '{event.keysym}'", justify="center")
             currentbinding = {"deviceGUID": KEYBOARD_GUID, "buttonIndex": event.keysym}
-    
+
     window.bind("<Key>", KeyboardEvent)
     window.protocol("WM_DELETE_WINDOW", lambda: SaveBind())
-    
+
     def GetDistanceFromDefault(currentVal, defaultVal):
         return abs(currentVal - defaultVal)
-    
+
     ignoredAxis = []
     ignoredButtons = []
-    
+
     foundAxis = False
     while not save:
         # Check if any of the states change
@@ -282,12 +341,12 @@ def ChangeKeybind(name:str, callback=None):
                         if ignore:
                             ignoredButtons.append(j)
                             ignore = False
-                            label.config(text=f"Listening for input...\n(expecting a button)")
+                            label.config(text=f"Listening for input...\n(expecting a button)", justify="center")
                             continue
                         if j in ignoredButtons:
                             continue
-                        
-                        label.config(text=f"Button: {j}")
+
+                        label.config(text=f"Detected a button press\nButton: {j}", justify="center")
                         currentbinding = {"deviceGUID": joystick.get_guid(), "buttonIndex": j}
 
             if keybindToChange["shouldBeAxis"]:    
@@ -297,14 +356,14 @@ def ChangeKeybind(name:str, callback=None):
                             print("Ignoring axis " + str(j))    
                             ignoredAxis.append(j)
                             ignore = False
-                            label.config(text=f"Listening for input...\n(expecting an axis)")
+                            label.config(text=f"Listening for input...\n(expecting an axis)", justify="center")
                             axisSlider.set(0)
                             continue
                         if j in ignoredAxis:
                             print("Ignoring axis " + str(j))
                             continue
-                        
-                        label.config(text=f"Axis: {j}")
+
+                        label.config(text=f"Detected an axis movement\nAxis: {j}", justify="center")
                         axisSlider.set(joystick.get_axis(j))
                         currentbinding = {"deviceGUID": joystick.get_guid(), "axisIndex": j}
                         foundAxis = True
@@ -314,7 +373,7 @@ def ChangeKeybind(name:str, callback=None):
 
         window.update()
         foundAxis = False
-        
+
     if currentbinding != None:
         SaveKeybind(name, deviceGUID=currentbinding["deviceGUID"], buttonIndex=currentbinding["buttonIndex"] if "buttonIndex" in currentbinding else -1, axisIndex=currentbinding["axisIndex"] if "axisIndex" in currentbinding else -1)
         KEYBINDS[KEYBINDS.index(next((item for item in KEYBINDS if item["name"] == name), None))] = {"name": name, 
@@ -328,7 +387,6 @@ def ChangeKeybind(name:str, callback=None):
 
         print(f"Saved keybind {name}")
 
-        
     if callback != None:
         callback()
         
