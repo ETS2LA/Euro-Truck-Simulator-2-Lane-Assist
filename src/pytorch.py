7c1b5a4cd26b3ee34eb73eeab68b7cba6ec11c7c
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-07-28 21:25:38 +0200

    Added instructions on how to downgrade pytorch when encountering the problem with loading fbgemm.dll or one of its dependencies.

--- src/pytorch.py
+++ src/pytorch.py
@@ -21,21 +21,39 @@ def CheckPyTorch():
         for module in modules.splitlines():
             if "torch " in module:
                 if "cu" in module:
-                    print(RED + "'torch' is installed with CUDA, please check yourself why the app is not working." + NORMAL)
+                    try:
+                        import torch
+                    except Exception as ex:
+                        if "fbgemm.dll" in str(ex):
+                            print(RED + "'torch' is installed with CUDA, a possible fix might be to do the following things:" + NORMAL + "\n > Run the activate.bat in " + path.replace('\\', '/') + "\n > Run the command 'pip uninstall torch torchvision torchaudio -y'\n > Run the command 'pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121'\n > Restart the app\n")
+                        else:
+                            print(RED + "'torch' is installed with CUDA, please check yourself or ask us on discord why pytorch with CUDA is not working." + NORMAL)
                     console.RestoreConsole()
                 else:
                     print(GREEN + "The app is working on a fix for the problem with 'torch', please don't close the app." + NORMAL)
                     threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch --upgrade", "torch")).start()
             elif "torchvision " in module:
                 if "cu" in module:
-                    print(RED + "'torchvision' is installed with CUDA, please check yourself why the app is not working." + NORMAL)
+                    try:
+                        import torchvision
+                    except Exception as ex:
+                        if "fbgemm.dll" in str(ex):
+                            print(RED + "'torchvision' is installed with CUDA, a possible fix might be to do the following things:" + NORMAL + "\n > Run the activate.bat in " + path.replace('\\', '/') + "\n > Run the command 'pip uninstall torch torchvision torchaudio -y'\n > Run the command 'pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121'\n > Restart the app\n")
+                        else:
+                            print(RED + "'torchvision' is installed with CUDA, please check yourself or ask us on discord why pytorch with CUDA is not working." + NORMAL)
                     console.RestoreConsole()
                 else:
                     print(GREEN + "The app is working on a fix for the problem with 'torchvision', please don't close the app." + NORMAL)
                     threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torchvision --upgrade", "torchvision")).start()
             elif "torchaudio " in module:
                 if "cu" in module:
-                    print(RED + "'torchaudio' is installed with CUDA, please check yourself why the app is not working." + NORMAL)
+                    try:
+                        import torchaudio
+                    except Exception as ex:
+                        if "fbgemm.dll" in str(ex):
+                            print(RED + "'torchaudio' is installed with CUDA, a possible fix might be to do the following things:" + NORMAL + "\n > Run the activate.bat in " + path.replace('\\', '/') + "\n > Run the command 'pip uninstall torch torchvision torchaudio -y'\n > Run the command 'pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121'\n > Restart the app\n")
+                        else:
+                            print(RED + "'torchaudio' is installed with CUDA, please check yourself or ask us on discord why pytorch with CUDA is not working." + NORMAL)
                     console.RestoreConsole()
                 else:
                     print(GREEN + "The app is working on a fix for the problem with 'torchaudio', please don't close the app." + NORMAL)
