597b500880cdf3a6126112beebd2d0e40cb24db2
DylDev <dylanb92010@gmail.com>
2024-08-04 15:53:34 -0400

    Updates to feeback

--- frontend/src/pages/feedback/index.tsx
+++ frontend/src/pages/feedback/index.tsx
@@ -1,34 +1,43 @@
 "use client"
 
 import { Card } from "@/components/ui/card"
-import { Textarea } from "@/components/ui/textarea"
-import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import Tiptap from "@/components/tiptap"
 import { SendCrashReport } from "../server"
 import { useState } from "react"
-import { Editor } from "@tiptap/react"
 import { toast } from "sonner"
 
 export default function Home() {
-    const [text, setText] = useState<string>("xd")
+    const [text, setText] = useState<string>("")
 
     const onUpdate = (editor: any) => {
         setText(editor.editor.getText());
     }
 
     const onSubmit = () => {
-        toast.promise(SendCrashReport("Feedback", text, "Sent from 2.0 frontend"))
+        if (text === "" || text === " ") {
+            toast.error("Feedback is empty, please provide any feedback you have.")
+            return
+        }
+        toast.promise(SendCrashReport("Feedback", text, "Sent from 2.0 frontend"), {
+            loading: "Sending feedback...",
+            success: "Feedback sent!",
+            error: "there was an error sending the feedback, try again later.",
+        })
     }
 
     return (
-        <Card className="flex flex-col content-center text-center pt-10 space-y-5 pb-0 h-[calc(100vh-72px)] overflow-auto w-full">
+        <Card className="flex flex-col content-center text-center h-[calc(100vh-72px)] overflow-auto">
             <div className="flex flex-col justify-self-center h-full w-full justify-center items-center gap-2">
-                <Label className="">Feedback</Label>
+                <div className="w-[800px]">
+                    <h3>Feedback</h3>
+                    <p className="text-zinc-600">Please provide any feedback you have. This will be send directly to a private channel for developers to see.</p>
+                    <p className="text-zinc-600">Do not share any personal information.</p>
+                </div>
                 <div className="w-[800px] h-80 border rounded-lg p-2">
                     <Tiptap className="w-full h-full text-start overflow-auto font-semibold" onUpdate={onUpdate} />
                 </div>
-                <Button className="w-[200px]" onClick={onSubmit}>Submit</Button>
+                <Button className="w-[800px] mt-2" onClick={onSubmit}>Submit</Button>
             </div>
         </Card>
     )
