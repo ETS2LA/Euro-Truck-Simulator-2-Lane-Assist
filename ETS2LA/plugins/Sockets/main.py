ea5304d414eda86164a69fa2b340a7ff84a06702
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-08-15 23:08:03 +0300

    Godot updates

--- ETS2LA/plugins/Sockets/main.py
+++ ETS2LA/plugins/Sockets/main.py
@@ -70,6 +70,11 @@ def speed(data):
     send += "speedLimit:" + str(data["truckFloat"]["speedLimit"]) + ";"
     return send
 
+def accelBrake(data):
+    send = "accel:" + str(data["truckFloat"]["gameThrottle"]) + ";"
+    send += "brake:" + str(data["truckFloat"]["gameBrake"]) + ";"
+    return send
+
 lastVehicles = [""]
 lastVehicleString = ""
 def vehicles(data):
@@ -151,6 +156,7 @@ def plugin():
     tempSend = ""
     tempSend += position(data)
     tempSend += speed(data)
+    tempSend += accelBrake(data)
     tempSend += vehicles(data)
     tempSend += traffic_lights(data)
     tempSend += steering(data)
