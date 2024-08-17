0b33b34d4e2ee4d3d9d9886f5af2c8e75737755d
Cloud hayes <Cloud@gproconnect.com>
2024-08-17 14:48:32 -0500

    Reapply "2.0 Linux Support"
    This reverts commit 65d4f5ecb3cb7e65efd41a53887885c5e3abba8c.

--- ETS2LA/modules/ScreenCapture/main.py
+++ ETS2LA/modules/ScreenCapture/main.py
@@ -5,6 +5,7 @@ from typing import Literal
 import numpy as np
 import cv2
 import mss
+import os
 
 runner:PluginRunner = None
 
@@ -17,6 +18,9 @@ monitor_y1 = monitor["top"]
 monitor_x2 = monitor["width"]
 monitor_y2 = monitor["height"]
 cam = None
+
+#Check if on linux
+LINUX = os.path.exists("/etc/os-release")
 
 def CreateCam(CamSetupDisplay:int = display):
     if variables.OS == "nt":
@@ -95,7 +99,10 @@ else:
         """imgtype: "both", "cropped", "full" """
         try:
             # Capture the entire screen
-            fullMonitor = sct.monitors[(display + 1)]
+            if LINUX:
+                fullMonitor = sct.monitors[display]
+            else:
+                fullMonitor = sct.monitors[(display) + 1]
             img = np.array(sct.grab(fullMonitor))
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             # return the requestet image, only crop when needed
