04b6d074a8a66392f8a2aa604f06fb1c83825b30
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-08-24 18:34:26 +0300

    Build lane rendering changes

--- godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
+++ godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1724504091|7708410476';
+const CACHE_VERSION = '1724513656|17273230429';
 /** @type {string} */
 const CACHE_PREFIX = 'ETS2LA Visualisa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
