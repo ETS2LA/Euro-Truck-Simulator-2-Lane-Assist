f2aa184788c7a0f0222379706180d080af34f125
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-08-25 02:30:42 +0200

    Update pytorch.py

--- ETS2LA/utils/pytorch.py
+++ ETS2LA/utils/pytorch.py
@@ -1,5 +1,5 @@
-import ETS2LA.variables as variables
 import ETS2LA.utils.console as console
+import ETS2LA.variables as variables
 import subprocess
 import threading
 import time
@@ -12,16 +12,17 @@ NORMAL = "\033[0m"
 ALLOW_INSTALL = True
 
 def CheckPyTorch():
-    path = os.path.dirname(os.path.dirname(variables.PATH)) + "/"
+    path = os.path.dirname(os.path.dirname(os.path.dirname(variables.PATH))) + "/"
 
     def UpdatePyTorch(command, module):
         global ALLOW_INSTALL
         while ALLOW_INSTALL == False:
             time.sleep(0.1)
         ALLOW_INSTALL = False
+        print(GREEN + f"\nThe app is working on a fix for the problem with '{module}', please don't close the app, it could take a few minutes." + NORMAL)
         command = str(command).replace("/", "\\")
-        subprocess.run(command, shell=True, stdout=subprocess.DEVNULL)
-        print(GREEN + f"'{module}' has been updated." + NORMAL)
+        subprocess.run(command, shell=True)
+        print(GREEN + f"'{module}' has been updated.\n" + NORMAL)
         ALLOW_INSTALL = True
 
     if os.path.exists(path + "venv/Scripts/activate.bat"):
@@ -34,55 +35,28 @@ def CheckPyTorch():
             if "torch " in module:
                 torch_found = True
                 if "cu" in module:
-                    try:
-                        import torch
-                    except Exception as ex:
-                        if "fbgemm.dll" in str(ex):
-                            print(RED + "'torch' is installed with CUDA, a possible fix might be to do the following things:" + NORMAL + "\n > Run the activate.bat in " + path.replace('\\', '/') + "\n > Run the command " + GREEN + "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121 --force-reinstall" + NORMAL + "\n > Restart the app\n")
-                        else:
-                            print(RED + "'torch' is installed with CUDA, please check yourself or ask us on discord why pytorch with CUDA is not working." + NORMAL)
-                    console.RestoreConsole()
+                    threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch==2.3.1 --index-url https://download.pytorch.org/whl/cu121 --force-reinstall", "torchaudio")).start()
                 else:
-                    print(GREEN + "The app is working on a fix for the problem with 'torch', please don't close the app, it could take a few minutes." + NORMAL)
-                    threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --force-reinstall", "torch")).start()
+                    threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch==2.3.1 --force-reinstall", "torch")).start()
             elif "torchvision " in module:
                 torchvision_found = True
                 if "cu" in module:
-                    try:
-                        import torchvision
-                    except Exception as ex:
-                        if "fbgemm.dll" in str(ex):
-                            print(RED + "'torchvision' is installed with CUDA, a possible fix might be to do the following things:" + NORMAL + "\n > Run the activate.bat in " + path.replace('\\', '/') + "\n > Run the command " + GREEN + "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121 --force-reinstall" + NORMAL + "\n > Restart the app\n")
-                        else:
-                            print(RED + "'torchvision' is installed with CUDA, please check yourself or ask us on discord why pytorch with CUDA is not working." + NORMAL)
-                    console.RestoreConsole()
+                    threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torchvision==0.18.1 --index-url https://download.pytorch.org/whl/cu121 --force-reinstall", "torchaudio")).start()
                 else:
-                    print(GREEN + "The app is working on a fix for the problem with 'torchvision', please don't close the app, it could take a few minutes." + NORMAL)
-                    threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --force-reinstall", "torchvision")).start()
+                    threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torchvision==0.18.1 --force-reinstall", "torchvision")).start()
             elif "torchaudio " in module:
                 torchaudio_found = True
                 if "cu" in module:
-                    try:
-                        import torchaudio
-                    except Exception as ex:
-                        if "fbgemm.dll" in str(ex):
-                            print(RED + "'torchaudio' is installed with CUDA, a possible fix might be to do the following things:" + NORMAL + "\n > Run the activate.bat in " + path.replace('\\', '/') + "\n > Run the command " + GREEN + "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121 --force-reinstall" + NORMAL + "\n > Restart the app\n")
-                        else:
-                            print(RED + "'torchaudio' is installed with CUDA, please check yourself or ask us on discord why pytorch with CUDA is not working." + NORMAL)
-                    console.RestoreConsole()
+                    threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121 --force-reinstall", "torchaudio")).start()
                 else:
-                    print(GREEN + "The app is working on a fix for the problem with 'torchaudio', please don't close the app, it could take a few minutes." + NORMAL)
-                    threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --force-reinstall", "torchaudio")).start()
+                    threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torchaudio==2.3.1 --force-reinstall", "torchaudio")).start()
 
         if torch_found == False:
-            print(GREEN + "The app is working on a fix for the problem with 'torch', please don't close the app, it could take a few minutes." + NORMAL)
-            threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --force-reinstall", "torch")).start()
+            threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch==2.3.1 --force-reinstall", "torch")).start()
         if torchvision_found == False:
-            print(GREEN + "The app is working on a fix for the problem with 'torchvision', please don't close the app, it could take a few minutes." + NORMAL)
-            threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --force-reinstall", "torchvision")).start()
+            threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torchvision==0.18.1 --force-reinstall", "torchvision")).start()
         if torchaudio_found == False:
-            print(GREEN + "The app is working on a fix for the problem with 'torchaudio', please don't close the app, it could take a few minutes." + NORMAL)
-            threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --force-reinstall", "torchaudio")).start()
+            threading.Thread(target=UpdatePyTorch, args=("cd " + path + "venv/Scripts & .\\activate.bat & cd " + path + " & pip install torchaudio==2.3.1 --force-reinstall", "torchaudio")).start()
 
     else:
 
