940b6e9d3ddc6469548ea933d78ab1578262faf7
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-09-12 19:29:10 +0300

    Basic mode map works again

--- frontend/src/components/map.tsx
+++ frontend/src/components/map.tsx
@@ -13,7 +13,7 @@ import {
 } from "@/components/ui/resizable"
 import { toast } from "sonner"
 import { useRouter } from "next/router"
-import { use, useState } from "react"
+import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { useTheme } from "next-themes"
 import { useEffect } from 'react'
@@ -21,45 +21,19 @@ import { set } from 'date-fns'
 import { useRef } from 'react'
 import CustomMarker from './custom_marker'
 import pako from 'pako';
-import maplibregl from 'maplibre-gl';
+import "leaflet-rotate"
 
-// Import maplibre css
-import 'maplibre-gl/dist/maplibre-gl.css';
 // Import leaflet css
-// import 'leaflet/dist/leaflet.css'
-
-function game_coord_to_coordinates(xx: number, yy: number): [number, number] {
-    // Values from TileMapInfo.json - the bounds of your game world in game coordinates
-    const x1 = -94621.8047;
-    const x2 = 79370.13;
-    const y1 = -80209.1641;
-    const y2 = 93782.77;
-
-    // The target lat/lon bounds that the game world corresponds to
-    const latMin = -90; // Replace with the correct bounds for your map region
-    const latMax = 90;  // Replace with the correct bounds for your map region
-    const lonMin = -180;         // Replace with the correct bounds for your map region
-    const lonMax = 180;          // Replace with the correct bounds for your map region
-
-    // Convert the game coordinates to relative positions
-    let xrel = (xx - x1) / (x2 - x1);  // Relative X position
-    let yrel = (yy - y1) / (y2 - y1);  // Relative Y position
-
-    // Map the relative positions to the latitude and longitude bounds
-    const lon = lonMin + xrel * (lonMax - lonMin);
-    const lat = latMin + yrel * (latMax - latMin);
-
-    return [lon, -(lat)]; // Return the calculated coordinates
-}
+import 'leaflet/dist/leaflet.css'
 
 // Component to update the map's view
-const UpdateMapView = ({ position, map }: { position: any, map: any }) => {
-    if (!map) return null; // If the map is not ready yet, do not render anything
-
-    position = game_coord_to_coordinates(position[0], position[1]); // Convert the game coordinates to normal map coordinates (lat, lng)
-    //console.log("Position:", position);
+const UpdateMapView = ({ position }: { position: LatLngTuple }) => {
+    const map = useMap(); // Access the map instance
     useEffect(() => {
-        map.flyTo({ center: position, zoom: 7.9 });
+        map.flyTo(position, 8, {
+            animate: false,
+            duration: 0
+        });
     }, [position, map]);
   
     return null; // This component does not render anything
@@ -91,24 +65,9 @@ let socket: WebSocket | null = null;
 export default function ETS2LAMap({ip} : {ip: string}) {
     const {theme, setTheme} = useTheme()
     const [connected, setConnected] = useState(false)
-    const [position, setPosition] = useState([0, 0])
+    const [position, setPosition] = useState<LatLngTuple>([0, 0])
     const [rotation, setRotation] = useState(0)
     const markerRef = useRef<L.Marker>(null);
-    const mapContainer = useRef<any>(null);
-    const map = useRef<any>(null);
-    const zoom = 7;
-
-    useEffect(() => {
-        if (map.current) return; // stops map from intializing more than once
-        console.log("Initializing map")
-        map.current = new maplibregl.Map({
-          container: mapContainer.current,
-          style: ip === "localhost" ? 'http://localhost:37520/api/map/style' : `http://${ip}:37520/api/map/style`,
-          center: [0, 0],
-          zoom: zoom
-        });
-        console.log("Map initialized")
-    }, [zoom]);
 
     useEffect(() => {
 
@@ -133,7 +92,6 @@ export default function ETS2LAMap({ip} : {ip: string}) {
                     // Use the Blob.arrayBuffer() method to read the Blob's content
                     const arrayBuffer = data.arrayBuffer().then(buffer => {
                         data = new Uint8Array(buffer);
-                        //console.log("Received message:", data);
                         const message = pako.ungzip(data, { windowBits: 28, to: 'string' });
                         const indices = message.split(";");
                         // Find X and Z
@@ -151,13 +109,12 @@ export default function ETS2LAMap({ip} : {ip: string}) {
                             }
                         });
                         if (!isNaN(X) && !isNaN(Z)) {
-                            //[X, Z] = game_coord_to_image(X, Z);
-                            setPosition([X, Z]);
+                            [X, Z] = game_coord_to_image(X, Z);
+                            setPosition([-Z, X]);
                         }
                     });
                 }
                 else{
-                    //console.log("Received message:", data);
                     const message = pako.ungzip(data, { windowBits: 28, to: 'string' });
                     const indices = message.split(";");
                     // Find X and Z
@@ -175,8 +132,8 @@ export default function ETS2LAMap({ip} : {ip: string}) {
                         }
                     });
                     if (!isNaN(X) && !isNaN(Z)) {
-                        //[X, Z] = game_coord_to_image(X, Z);
-                        setPosition([X, Z]);
+                        [X, Z] = game_coord_to_image(X, Z);
+                        setPosition([-Z, X]);
                     }
                 }
 
@@ -224,25 +181,25 @@ export default function ETS2LAMap({ip} : {ip: string}) {
             }
         }
     }, [rotation]); // This effect depends on the rotation state    
+
+    let corner1 = latLng(0, 165168)
+    let corner2 = latLng(148512, 0)
+    let bounds = latLngBounds(corner1, corner2)
     // <CustomMarker position={position} rotation={rotation} />
-    // <MapContainer center={position} zoom={7} style={{height: "100%", width: "100%", backgroundColor: "#1b1b1b"}} zoomControl={false} bounds={bounds} crs={CRS.Simple}>
-    //    <TileLayer
-    //    attribution='&copy; ETS2LA Team'
-    //    url="https://raw.githubusercontent.com/ETS2LA/tilemap/master/tilemap/Tiles/{z}/{x}/{y}.png"
-    //    minNativeZoom={2}
-    //    maxNativeZoom={8}
-    //    zIndex={-999}
-    //    tileSize={512}
-    //    />
-    //    <CustomMarker position={position} rotation={rotation} />
-    //    <UpdateMapView position={position} />
-    //</MapContainer>
     return (
-        <div className='w-full h-full'>
-            <div ref={mapContainer} className='w-full h-full' style={{backgroundColor: "#1b1b1b"}}>
-                <UpdateMapView position={position} map={map.current} />
-            </div>
-            <div className='border absolute w-2 h-2 top-[calc(50vh-0.5rem)] left-[calc(35vw-1.5rem)] border-white' />
-        </div>
+        <>
+            <MapContainer center={position} zoom={7} style={{height: "100%", width: "100%", backgroundColor: "#1b1b1b"}} zoomControl={false} bounds={bounds} crs={CRS.Simple} rotate={true}>
+                <TileLayer
+                    attribution='&copy; ETS2LA Team'
+                    url="https://raw.githubusercontent.com/ETS2LA/tilemap/master/tilemap/Tiles/{z}/{x}/{y}.png"
+                    minNativeZoom={2}
+                    maxNativeZoom={8}
+                    zIndex={-999}
+                    tileSize={512}
+                />
+                <CustomMarker position={position} rotation={rotation} />
+                <UpdateMapView position={position} />
+            </MapContainer>
+        </>
     )
 }
