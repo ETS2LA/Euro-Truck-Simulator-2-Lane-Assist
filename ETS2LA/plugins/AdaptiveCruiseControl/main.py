5a79f443a382ea1820277d081ec9f846a843fd64
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-09-30 22:55:54 +0300

    Fix a possible error in the ACC plugin

--- ETS2LA/plugins/AdaptiveCruiseControl/main.py
+++ ETS2LA/plugins/AdaptiveCruiseControl/main.py
@@ -132,8 +132,10 @@ def GetTimeToVehicleAhead(apiData: dict) -> float:
         
     if len(points) == 2:
         # Generate 10 points between the two points
-        x1, y1 = points[0]
-        x2, y2 = points[1]
+        x1 = points[0][0]
+        y1 = points[0][1]
+        x2 = points[1][0]
+        y2 = points[1][1]
         points = [[x1 + (x2 - x1) * i / 10, y1 + (y2 - y1) * i / 10] for i in range(10)]
     
     if type(vehicles) != list:
