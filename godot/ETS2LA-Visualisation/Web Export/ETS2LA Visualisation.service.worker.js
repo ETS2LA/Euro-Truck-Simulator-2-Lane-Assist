397a2937db08610fc5e0fc158a1e4a3a739dfd02
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-10-10 00:15:21 +0300

    Double maximum rendered objects in godot.

--- godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
+++ godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728506844|2511260105';
+const CACHE_VERSION = '1728508435|63337457';
 /** @type {string} */
 const CACHE_PREFIX = 'ETS2LA Visualisa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
