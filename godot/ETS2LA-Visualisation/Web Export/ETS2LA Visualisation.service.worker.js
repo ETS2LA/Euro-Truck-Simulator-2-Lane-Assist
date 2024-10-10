05d5720e1a9ee53782582e67104136e587b33252
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-10-10 23:00:01 +0300

    Fix short render distance

--- godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
+++ godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728590154|1933677284';
+const CACHE_VERSION = '1728590386|35420414';
 /** @type {string} */
 const CACHE_PREFIX = 'ETS2LA Visualisa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
