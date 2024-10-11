fa43d3ea8a763f761af0a9958e81bbb4f5a6cb03
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-10-11 10:21:12 +0300

    Optimized godot visualisation, now 33% faster.

--- godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
+++ godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728630589|46988709';
+const CACHE_VERSION = '1728631252|468208931';
 /** @type {string} */
 const CACHE_PREFIX = 'ETS2LA Visualisa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
