af2a128f07bcde45b20567399802ac8b4dbe0d5a
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-10-30 16:13:00 +0200

    Build godot with last update stuff

--- godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
+++ godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1730201003|2035627754';
+const CACHE_VERSION = '1730297570|5106813111';
 /** @type {string} */
 const CACHE_PREFIX = 'ETS2LA Visualisa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
