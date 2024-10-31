9c756c9396e5bd193c969802e008297b2e902f78
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-10-31 18:05:19 +0200

    NPC trucks now have their models

--- godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
+++ godot/ETS2LA-Visualisation/Web Export/ETS2LA Visualisation.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1730390431|3214982462';
+const CACHE_VERSION = '1730390711|3494112569';
 /** @type {string} */
 const CACHE_PREFIX = 'ETS2LA Visualisa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
