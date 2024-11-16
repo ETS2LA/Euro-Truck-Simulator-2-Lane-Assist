0f0fa94ec0979cf62639c382c8ff5e290cb740cb
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-11-16 16:31:47 +0200

    Dashboard will push to about

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,10 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+    const { push } = useRouter();
+    push("/about");
     return (
         <div>
             
