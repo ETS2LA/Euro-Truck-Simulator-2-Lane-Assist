d71251ac37a30afe983a4f69c9f5b791a4412896
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2024-11-17 13:14:23 +0200

    Add about page translation keys

--- pages/about.py
+++ pages/about.py
@@ -1,28 +1,28 @@
 from ETS2LA.backend import settings
 from ETS2LA.UI import *
 
+from ETS2LA.utils.translator import Translate
 import ETS2LA.utils.translator as translator
-import webbrowser
 import time
 
 contributors = [
-    {"name": "Tumppi066", "description": "Lead developer and creator of ETS2LA", "links": [["Github", "https://github.com/Tumppi066"], ["Youtube", "https://www.youtube.com/@Tumppi066"], ["Ko-Fi", "https://ko-fi.com/tumppi066"]]},
-    {"name": "Glas42", "description": 'Navigation Detection, Traffic Light Detection, ETS2LA Lite, "co-owner"', "links": [["Github", "https://github.com/Glas42"]]},
-    {"name": "Cloud", "description": "Linux & Unix Port, various improvements and bug fixes", "links": []},
-    {"name": "DylDev", "description": "Various additions and improvements, Object Detection AI models & development", "links": [["Github", "https://github.com/DylDevs"], ["Youtube", "https://www.youtube.com/@DylDev"]]},
-    {"name": "truckersmudgeon", "description": "Game data extraction and processing", "links": []},
-    {"name": "DTheIcyDragon", "description": "Bug fixes", "links": []},
-    {"name": "Roman Sonnik", "description": "Bug fixes", "links": [["Github", "https://github.com/RomanSonnik"]]},
-    {"name": "Lun", "description": "Chinese translation, bug fixes", "links": [["Discord", "https://discordapp.com/users/832636302402256898"]]},
-    {"name": "atac_helicopter", "description": "Bug fixes", "links": []},
-    {"name": "ғʟᴇxғʟᴇxᴇɴ", "description": "Bug fixes", "links": []},
-    {"name": "LookAtYourSkill", "description": "Bug fixes", "links": []},
-    {"name": "mimi89999", "description": "scs-sdk-controller developer", "links": []},
-    {"name": "zhaoyj", "description": "3D models for the visualization", "links": []},
-    {"name": "MRUIAW", "description": "Bug fixes, Chinese translations", "links": []},
-    {"name": "ViSzKe", "description": "Bug fixes", "links": []},
-    {"name": "ziakhan4505", "description": "C++ support, Linux & Unix Port, bug fixes", "links": []},
-    {"name": "Piotrke", "description": "Game hooks", "links": []},
+    {"name": "Tumppi066", "description": Translate("about.tumppi066.description"), "links": [["Github", "https://github.com/Tumppi066"], ["Youtube", "https://www.youtube.com/@Tumppi066"], ["Ko-Fi", "https://ko-fi.com/tumppi066"]]},
+    {"name": "Glas42", "description": Translate("about.glas42.description"), "links": [["Github", "https://github.com/Glas42"]]},
+    {"name": "Cloud", "description": Translate("about.cloud.description"), "links": []},
+    {"name": "DylDev", "description": Translate("about.dyldev.description"), "links": [["Github", "https://github.com/DylDevs"], ["Youtube", "https://www.youtube.com/@DylDev"]]},
+    {"name": "truckersmudgeon", "description": Translate("about.truckersmudgeon.description"), "links": []},
+    {"name": "mimi89999", "description": Translate("about.mimi89999.description"), "links": []},
+    {"name": "zhaoyj", "description": Translate("about.zhaoyj.description"), "links": []},
+    {"name": "Lun", "description": Translate("about.lun.description"), "links": [["Discord", "https://discordapp.com/users/832636302402256898"]]},
+    {"name": "ziakhan4505", "description": Translate("about.ziakhan4505.description"), "links": []},
+    {"name": "MRUIAW", "description": Translate("about.mruiaw.description"), "links": []},
+    {"name": "Piotrke", "description": Translate("about.piotrke.description"), "links": []},
+    {"name": "DTheIcyDragon", "description": Translate("about.dtheicydragon.description"), "links": []},
+    {"name": "Roman Sonnik", "description": Translate("about.romansonnik.description"), "links": [["Github", "https://github.com/RomanSonnik"]]},
+    {"name": "atac_helicopter", "description": Translate("about.atac_helicopter.description"), "links": []},
+    {"name": "ғʟᴇxғʟᴇxᴇɴ", "description": Translate("about.flexflexen.description"), "links": []},
+    {"name": "LookAtYourSkill", "description": Translate("about.lookatyourskill.description"), "links": []},
+    {"name": "ViSzKe", "description": Translate("about.viszke.description"), "links": []},
 ]
 
 class Page(ETS2LAPage):
@@ -35,11 +35,11 @@ class Page(ETS2LAPage):
             Space(20)
             with Padding(20):
                 with Group("vertical"):
-                    Title("About")
-                    Description("ETS2LA is a project that aims to provide an easy to use self driving solution for ETS2 and ATS, if you want to learn more then you can visit the github page or the wiki via the sidebar.")
+                    Title(Translate("about.about"))
+                    Description(Translate("about.description"))
                     Space(2)
                     with Group("vertical", padding=0):
-                        Title("Developers / Contributors")
+                        Title(Translate("about.developers"))
 
                         for contributor in contributors:
                             with Group("vertical", gap=6, padding=0):
@@ -51,7 +51,7 @@ class Page(ETS2LAPage):
                     
                     Space(12)
                     with Group("vertical", padding=0, gap=24):
-                        Title("Translation Credits")
+                        Title(Translate("about.translation_credits"))
                         
                         for language in translator.LANGUAGES:
                             with Group("vertical", gap=6, padding=0):
@@ -60,21 +60,21 @@ class Page(ETS2LAPage):
                                     Description("(" + translator.TranslateToLanguage("name_en", translator.GetCodeForLanguage(language)) + ")", size="xs")
                                 credits = translator.TranslateToLanguage("language_credits", translator.GetCodeForLanguage(language))
                                 if language != "English" and credits == translator.TranslateToLanguage("language_credits", translator.GetCodeForLanguage("English")):
-                                    credits = "Language has no credits."
+                                    credits = Translate("about.no_credits")
                                 Description(credits)
                                 
                     Space(12)
                     with Group("vertical", padding=0, gap=16):
-                        Title("Support Development")
+                        Title(Translate("about.support_development"))
                         with Group("vertical", gap=6, padding=0):
-                            Description("• If you like the project and want to support the development, you can do so by donating via Ko-Fi.")
+                            Description(Translate("about.kofi_description"))
                             Link("  Ko-Fi", "https://ko-fi.com/tumppi066")
                         with Group("vertical", gap=6, padding=0):
-                            Description("• If you want to contribute, then I recommend joining the discord server and checking out the github page.")
+                            Description(Translate("about.contribute_description"))
                             Link("  Discord", "https://discord.gg/ETS2LA")
                             Link("  Github", "https://github.com/ETS2LA")
                         with Group("vertical", gap=6, padding=0):
-                            Description("• If you want to contribute to the translations, then you can do so by joining the discord.")
+                            Description(Translate("about.translate_description"))
                             Link("  Discord", "https://discord.gg/ETS2LA")
                     
         return RenderUI()
