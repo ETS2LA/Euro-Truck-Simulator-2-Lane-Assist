461481d4bff5a610e81504885607f479be91f337
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-12-31 17:46:11 +0100

    the update.bat now enters the venv automatically

--- update.bat
+++ update.bat
@@ -1,1 +1,8 @@
+set venv=%VIRTUAL_ENV%
+set venv_path=..\..\venv\Scripts\activate.bat
+if "%venv%" == "" (
+    if exist "%venv_path%" (
+        call "%venv_path%"
+    )
+)
 python Updater/updater.py
