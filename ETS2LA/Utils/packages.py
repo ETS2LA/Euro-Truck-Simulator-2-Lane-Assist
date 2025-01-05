be53523f6b1ba6ba7701643859a379e059b1adeb
Sheng Fan <fredtools999@gmail.com>
2025-01-05 19:28:44 +0800

    fix(utils.packages): version comparison calculation

--- ETS2LA/Utils/packages.py
+++ ETS2LA/Utils/packages.py
@@ -1,3 +1,4 @@
+from typing import TypeAlias, Union
 from ETS2LA.Utils.Console.colors import *
 from ETS2LA.Utils.shell import ExecuteCommand
 import ETS2LA.Networking.cloud as cloud
@@ -22,12 +23,39 @@ def CheckForMaliciousPackages():
             cloud.SendCrashReport(package, "Update malicious package error.", traceback.format_exc())
             print(RED + f"Unable to check the version of the '{package}' package. Please update your '{package}' package manually if you have one of these versions installed: {malicious_packages[package]}" + END)
 
-def FixModule(module, ver, url):
+VersionType: TypeAlias = Union[
+    int,
+    tuple[int],
+    tuple[int, int],
+    tuple[int, int, int],
+    str,
+]
+
+def ConvertVersionToTuple(ver: VersionType) -> tuple:
+    if isinstance(ver, str):
+        return tuple(map(int, ver.split(".")))
+    elif isinstance(ver, int):
+        return (ver,)
+    elif isinstance(ver, tuple):
+        return ver
+    else:
+        raise ValueError(f"Invalid version type: {type(ver)}")
+
+def CompareVersions(ver1: VersionType, ver2: VersionType) -> int:
+    a, b = ConvertVersionToTuple(ver1), ConvertVersionToTuple(ver2)
+    if a < b:
+        return -1
+    elif a > b:
+        return 1
+    else:
+        return 0
+
+def FixModule(module: str, ver: VersionType, install_source: str):
     try:
         cur_ver = version(module)
     except:
         cur_ver = "0.0.0"
         
-    if cur_ver < ver:
+    if CompareVersions(cur_ver, ver) < 0:
         print(f"Please wait, we need to install the correct version of {module}...")
-        ExecuteCommand(f"pip install {url}")
+        ExecuteCommand(f"pip install {install_source}")
