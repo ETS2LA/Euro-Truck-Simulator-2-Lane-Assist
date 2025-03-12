a056694c1759c16031d3a47e0487f33a68b42880
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2025-03-12 14:23:19 +0200

    Only edit proxy when proxy_override is set.
    - This might fix hanging on github clones when a custom system wide proxy is set.

--- ETS2LA/Utils/shell.py
+++ ETS2LA/Utils/shell.py
@@ -69,19 +69,19 @@ def get_system_proxy_configuration() -> ProxyConfiguration | None:
 def ExecuteCommand(command: str,
                    proxy_override: ProxyConfiguration | None = None,
                    silent: bool = False) -> int:
-    # execute a command with HTTP_PROXY and HTTPS_PROXY environment variables set
+
     current_proxy = get_system_proxy_configuration()
+    env = os.environ.copy()
     if proxy_override is not None:
         current_proxy = proxy_override
 
-    env = os.environ.copy()
-    if current_proxy is not None:
-        if current_proxy.username is not None and current_proxy.password is not None:
-            proxy_str = f"{current_proxy.proto}://{current_proxy.username}:{current_proxy.password}@{current_proxy.host}:{current_proxy.port}"
-        else:
-            proxy_str = f"{current_proxy.proto}://{current_proxy.host}:{current_proxy.port}"
-        env["HTTP_PROXY"] = proxy_str
-        env["HTTPS_PROXY"] = proxy_str
+        if current_proxy is not None:
+            if current_proxy.username is not None and current_proxy.password is not None:
+                proxy_str = f"{current_proxy.proto}://{current_proxy.username}:{current_proxy.password}@{current_proxy.host}:{current_proxy.port}"
+            else:
+                proxy_str = f"{current_proxy.proto}://{current_proxy.host}:{current_proxy.port}"
+            env["HTTP_PROXY"] = proxy_str
+            env["HTTPS_PROXY"] = proxy_str
     
     if silent:
         result = subprocess.run(command, shell=True, env=env, capture_output=True)
