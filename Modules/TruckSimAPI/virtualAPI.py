1d9fb88147931a4760430ac69869afdab292002c
DylDev <dylanb92010@gmail.com>
2025-03-29 20:22:13 -0400

    Add Casper, Wyoming to VirtualAPI

--- Modules/TruckSimAPI/virtualAPI.py
+++ Modules/TruckSimAPI/virtualAPI.py
@@ -39,13 +39,17 @@ substanceSize = 25
 # virtualX = 44000
 # virtualZ = -49200
 
-# ATS (Los Angeles)
+# ATS (Los Angeles, California)
 #virtualX = -105000
 #virtualZ = 15000
 
-# ATS (Houston)
-virtualX = 2633
-virtualZ = 53979
+# ATS (Houston, Texas)
+#virtualX = 2633
+#virtualZ = 53979
+
+# ATS (Casper, Wyoming)
+virtualX = -42247
+virtualZ = -24765
 
 # Working Set (not a specific location)
 # virtualX = 23475
