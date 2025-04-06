f7d5de54c21b942092433ad78c6479f49f421548
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2025-04-06 18:02:18 +0300

    Fix typo in previous update

--- Plugins/Map/navigation/navigation.py
+++ Plugins/Map/navigation/navigation.py
@@ -112,7 +112,7 @@ def get_path_to_destination():
         logging.warning(f"Successfully calculated lanes for {sum(success)} out of {len(success)} nodes ({sum(success) / len(success) * 100:.0f}%)")
         data.navigation_plan = route
         nodes = [nav.node for nav in route]
-        data.plugin.tags.navigation_plan = nodes
+        data.plugin.globals.tags.navigation_plan = nodes
         data.last_length = len(game_route)
         
     return data.navigation_plan
