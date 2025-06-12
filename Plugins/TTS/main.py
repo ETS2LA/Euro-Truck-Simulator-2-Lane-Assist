bdacf99dab8f2c74f9446d6828fb658a5d3019c8
Piggywu981 <Piggywu@outlook.com>
2025-06-12 15:03:51 +0800

    Fix(TTS): fix wrong logic

--- Plugins/TTS/main.py
+++ Plugins/TTS/main.py
@@ -337,7 +337,7 @@ class Plugin(ETS2LAPlugin):
             route_distance = api["truckFloat"]["routeDistance"]
             
             for distance in self.notified_distances:
-                distance = distance * 1000 # Meters to Kilometers
+                distance = distance / 1000 # Meters to Kilometers
                 if route_distance <= distance and distance not in self.notified_markers:
                     if self.last_route_distance > 0 and route_distance < self.last_route_distance:
                         self.speak(Translate("tts.route.distance", [distance]))
