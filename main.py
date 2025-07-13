257b952e65b0c058a185a332bfcf4fdd6d6e3482
DylDev <dylanb92010@gmail.com>
2025-07-13 10:45:21 -0400

    Update ATS offsets
    Fixed a couple spots in the ATS game data, you should update it by going to Settings > Map > Data and clicking Update
    
    If you find any more offsets please report them in the Discord or make a GitHub issue so I can sort them out.

--- main.py
+++ main.py
@@ -242,4 +242,4 @@ if __name__ == "__main__":
 #         the cache of the app for changes that don't necessarily 
 #         happen inside of this repository (like the frontend).
 # 
-# Counter: 18
+# Counter: 19
