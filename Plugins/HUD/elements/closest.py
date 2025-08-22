ebb66c35a8943ed5a2e88bf284eea4d47f97f7c7
motionzlol <maxward645@gmail.com>
2025-08-22 21:38:15 +0100

    Added another AR HUD element to show the closest city.

---
+++ Plugins/HUD/elements/closest.py
@@ -0,0 +1,77 @@
+# TODO: get the exact distance from the truck to the closest city
+
+
+from Plugins.HUD.classes import HUDWidget
+from ETS2LA.Utils.translator import _
+from Plugins.AR.classes import *
+
+class Widget(HUDWidget):
+    name = _("Closest City")
+    description = _("Displays the closest city and its distance ( relative to the text ).")
+    fps = 2
+    
+    def __init__(self, plugin):
+        super().__init__(plugin)
+    
+    def settings(self):
+        return super().settings()
+        
+    def draw(self, offset_x, width, height=50):
+        if not self.plugin.data:
+            return
+        
+        closest_city = self.plugin.globals.tags.closest_city
+        closest_city_distance = self.plugin.globals.tags.closest_city_distance
+        closest_country = self.plugin.globals.tags.closest_country
+        
+        if closest_city:
+            closest_city = self.plugin.globals.tags.merge(closest_city)
+        else:
+            closest_city = _("Unknown")
+            
+        if closest_city_distance:
+            closest_city_distance = self.plugin.globals.tags.merge(closest_city_distance)
+        else:
+            closest_city_distance = 0
+            
+        if closest_country:
+            closest_country = self.plugin.globals.tags.merge(closest_country)
+        else:
+            closest_country = ""
+        
+        city_display = closest_city
+        if closest_country and closest_country != "":
+            city_display = f"{closest_city}, {closest_country}"
+        
+        if closest_country and "Great Britain" in closest_country:
+            corrected_distance = closest_city_distance / 15
+        else:
+            corrected_distance = closest_city_distance / 19
+        
+        distance_text = f"{corrected_distance:.1f} km"
+        
+        max_city_length = 20
+        if len(city_display) > max_city_length:
+            city_display = city_display[:max_city_length-3] + "..."
+
+        self.data = [
+            Rectangle(
+                Point(offset_x, 0, anchor=self.plugin.anchor),
+                Point(width + offset_x, height, anchor=self.plugin.anchor),
+                color=Color(255, 255, 255, 20),
+                fill=Color(255, 255, 255, 10),
+                rounding=6,
+            ),
+            Text(
+                Point(10 + offset_x, 6, anchor=self.plugin.anchor),
+                text=city_display,
+                color=Color(255, 255, 255, 200),
+                size=15
+            ),
+            Text(
+                Point(10 + offset_x, height-18, anchor=self.plugin.anchor),
+                text=distance_text,
+                color=Color(255, 255, 255, 200),
+                size=14
+            )
+        ]
