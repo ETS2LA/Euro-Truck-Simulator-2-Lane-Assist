f6a51d8e35941d7f5f4b6eebea9fe4e7fece9c27
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2025-12-04 16:38:46 +0200

    Add ETS2LA C# project template.

---
+++ .vscode/template/Program.cs
@@ -0,0 +1,27 @@
+using ETS2LA.Shared;
+
+[assembly: PluginInformation("MyPlugin", "Describe your plugin here.")]
+namespace MyPlugin
+{
+    public class MyPlugin : Plugin
+    {
+        public override void Init(IEventBus bus)
+        {
+            base.Init(bus);
+            // Initialization code here
+            // e.g., subscribe to events
+            // _bus?.Subscribe<YourEventType>("YourTopic", YourEventHandler);
+        }
+
+        public override void Tick()
+        {
+            // This will be run at the specified TickRate when the plugin is enabled.
+            // You can use `public override float TickRate => 10f;` to change the tick rate to whatever FPS you want.
+            // Please do not use a tick rate higher than what you need, that uses extra performance.
+
+            // If your code works exclusively off of events you can remove this override. In this case unless your code
+            // is purely a library, you should check if _IsRunning is true in your even handlers to avoid processing
+            // events when the user has set the plugin to disabled.
+        }
+    }
+}
