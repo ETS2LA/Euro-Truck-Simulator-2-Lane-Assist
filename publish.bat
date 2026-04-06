d99ff6112c0aded7c8db01cc462c23dfc7c960c8
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2026-04-06 18:19:15 +0300

    Fix plugins not being copied into the ETS2LA directory.

--- publish.bat
+++ publish.bat
@@ -1,3 +1,3 @@
 dotnet build ETS2LA.sln -c Release --no-incremental
-dotnet publish ETS2LA/ETS2LA.csproj --self-contained -r win-x64 -o .\publish
+dotnet publish ETS2LA/ETS2LA.csproj --self-contained -o .\publish
 xcopy /E /I /Y .\Assets .\publish\Assets
